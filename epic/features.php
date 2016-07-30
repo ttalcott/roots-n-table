6fc80c80457b62bb492df1669886338034b42b58
Travis Talcott <ttalcott@aquilagroup.com>
2016-07-30 14:48:33 -0600

    added header

--- epic/features.php
+++ epic/features.php
@@ -4,4 +4,18 @@
 		<meta charset="UTF-8" />
 		<title>Roots 'n Table Feature List</title>
 	</head>
+	<body>
+		<div class="header">
+			<header>
+				<h1>
+					Roots 'n Table Feature List
+				</h1>
+			</header>
+		</div>
+		<main>
+			<h3>
+				
+			</h3>
+		</main>
+	</body>
 </html>
