6d001332ed11964913a01ebc4ce3e796455064d3
Travis Talcott <ttalcott@aquilagroup.com>
2016-07-30 15:02:02 -0600

    added goals

--- epic/features.php
+++ epic/features.php
@@ -2,13 +2,13 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8" />
-		<title>Roots 'n Table Feature List</title>
+		<title>Roots 'n Table</title>
 	</head>
 	<body>
 		<div class="header">
 			<header>
 				<h1>
-					Roots 'n Table Feature List
+					Roots 'n Table
 				</h1>
 			</header>
 		</div>
@@ -18,10 +18,17 @@
 					Vision
 				</h3>
 				<p>
-					Roots 'n Table is a site that will bring local produce and other goods from local farmers and growers to the general public. This will benefit the community by keeping the agriculture business local and provide fresh and trustworthy products while enhancing the relationships between the families and their food providers.   
+					Roots 'n Table is a site that will bring local produce and other goods from local farmers and growers to the general public. This will benefit the community by keeping the agriculture business local and provide fresh and trustworthy products while enhancing the relationships between the families and their food providers.
 				</p>
 			</div>
-
+			<div class="goal">
+				<h3>
+					Goal of Roots 'n Table
+				</h3>
+				<p>
+					Our goal is to keep business local and to give people easy access to local food sources. We hope that our site will allow for the growth of the farmer's businesses by making their products easily accessible and also allow for smaller growers to be able to sell their goods.
+				</p>
+			</div>
 		</main>
 	</body>
 </html>
