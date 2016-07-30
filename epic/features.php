947b4fed0963216d30493b4aa14ce814cde8d6ee
Travis Talcott <ttalcott@aquilagroup.com>
2016-07-30 14:57:33 -0600

    added vision statement

--- epic/features.php
+++ epic/features.php
@@ -13,9 +13,15 @@
 			</header>
 		</div>
 		<main>
-			<h3>
-				
-			</h3>
+			<div class="vision">
+				<h3>
+					Vision
+				</h3>
+				<p>
+					Roots 'n Table is a site that will bring local produce and other goods from local farmers and growers to the general public. This will benefit the community by keeping the agriculture business local and provide fresh and trustworthy products while enhancing the relationships between the families and their food providers.   
+				</p>
+			</div>
+
 		</main>
 	</body>
 </html>
