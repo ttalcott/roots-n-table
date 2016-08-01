b777ed604963b67d26570915a2f0ff34775921cb
Travis Talcott <ttalcott@aquilagroup.com>
2016-08-01 15:57:23 -0600

    added some of a persona

--- epic/farmer-fuzzy.php
+++ epic/farmer-fuzzy.php
@@ -5,11 +5,19 @@
 		<meta charset="UTF-8" />
 	</head>
 	<body>
-		<div class="header>">
+		<div class="header">
 			<header>
-				<h1>Farmer Fuzzy Persona</h1>
+				<h1>Farmer Fuzzy</h1>
 			</header>
 		</div>
-		
+		<div class="persona">
+			<h3>Persona:</h3>
+			<ul>
+				<li>Age: 32 years old</li>
+				<li>Occupation: Professional farmer who makes their living selling their goods at farmers markets and also by selling their produce straight from their farm.</li>
+				<li>Products: Farmer Fuzzy grows tomatoes, potatoes, kale, radishes, corn, mushrooms, zucchini, carrots, eggplant, and grapes. He also raises chicken and has 3 cows who produce enough milk for him and his customers.</li>
+				<li>Goals: Farmer Fuzzy loves selling his goods at farmers markets but wants more people to reserve his goods before the market opens because he often sells out which angers some of his frequent customers. He also wants more people to realize that they can come buy his produce from his farm, and is willing to put together packages for them.</li>
+			</ul>
+		</div>
 	</body>
 </html>
