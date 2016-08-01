2afc254a943872c305716bc241e435052bf49214
Robert Engelbert <Robe32002@yahoo.com>
2016-08-01 16:59:08 -0600

    Added Frustrations for Maria the potato grower.

--- epic/potato-grower.php
+++ epic/potato-grower.php
@@ -7,5 +7,7 @@
 	<body>
 		<h1>Potato's and flower's</h1>
 		<p><strong>Maria Jones:</strong> a 36 year old gardener and stay at home mom that wants to earn an income. She uses a LG K7 Android phone and a HP Notebook laptop to access the internet. Maria will be using Roots n' table farmers market app to sell her Potato's and flower's</p>
+		<h2>Frustrations</h2>
+		<p>Maria is frustrated because she can't make it to the farmers market to sell her delicious potato's or beautiful flowers.</p>
 	</body>
 </html>
