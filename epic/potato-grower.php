2c0bd3570bf74004f9b9357007aaf52294cbe12c
Robert Engelbert <Robe32002@yahoo.com>
2016-08-01 16:55:06 -0600

    Added a persona for Maria the potato grower.

--- epic/potato-grower.php
+++ epic/potato-grower.php
@@ -6,6 +6,6 @@
 	</head>
 	<body>
 		<h1>Potato's and flower's</h1>
-		<p><strong></strong></p>
+		<p><strong>Maria Jones:</strong> a 36 year old gardener and stay at home mom that wants to earn an income. She uses a LG K7 Android phone and a HP Notebook laptop to access the internet. Maria will be using Roots n' table farmers market app to sell her Potato's and flower's</p>
 	</body>
 </html>
