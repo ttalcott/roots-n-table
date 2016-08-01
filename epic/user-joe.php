f9643d2bdd429df1ce288963399f8c88beab61f3
Robert Engelbert <Robe32002@yahoo.com>
2016-08-01 16:20:31 -0600

    Added a goal for Joe Dough's persona.

--- epic/user-joe.php
+++ epic/user-joe.php
@@ -7,5 +7,7 @@
 	<body>
 		<h1>Overworked Doctor</h1>
 		<p><strong>Joe Dough</strong> is a 30 year old Doctor, he's a health nut and he loves to buy local. He's very busy and rarely has time to make it to the farmers market. Joe is  always on his iphone throughout the day and uses a Macbook to surf the net at home</p>
+		<h2>Goal</h2>
+		<p>Joe's goal in using Roots n' table farmers market app, is to buy fresh locally grown produce over the internet and schedule a pick up time that's convenient for him.</p>
 	</body>
 </html>
