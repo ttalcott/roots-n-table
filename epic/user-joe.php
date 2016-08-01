87e53c08f30b2932d727d9da1d7efd603c70ff15
Robert Engelbert <Robe32002@yahoo.com>
2016-08-01 16:05:41 -0600

    Added a persona for Joe Dough

--- epic/user-joe.php
+++ epic/user-joe.php
@@ -5,5 +5,7 @@
 		<title>Joe's persona</title>
 	</head>
 	<body>
+		<h1>Overworked Doctor</h1>
+		<p><strong>Joe Dough</strong> is a 30 year old Doctor, he's a health nut and he loves to buy local. He's very busy and rarely has time to make it to the farmers market. Joe is  always on his iphone throughout the day and uses a Macbook to surf the net at home</p>
 	</body>
 </html>
