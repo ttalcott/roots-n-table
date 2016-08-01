ee0d513bbc4fb153ba8e778b6df353c669696ac4
Travis Talcott <ttalcott@aquilagroup.com>
2016-08-01 15:49:17 -0600

    added header

--- epic/farmer-fuzzy.php
+++ epic/farmer-fuzzy.php
@@ -4,5 +4,12 @@
 		<title>Farmer Fuzzy</title>
 		<meta charset="UTF-8" />
 	</head>
-	
+	<body>
+		<div class="header>">
+			<header>
+				<h1>Farmer Fuzzy Persona</h1>
+			</header>
+		</div>
+		
+	</body>
 </html>
