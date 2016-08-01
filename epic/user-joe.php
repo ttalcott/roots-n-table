58d9178223008b2f441e8f5c9958d75269fd183f
Robert Engelbert <Robe32002@yahoo.com>
2016-08-01 16:27:45 -0600

    Added Joe's frustration for his persona.

--- epic/user-joe.php
+++ epic/user-joe.php
@@ -7,6 +7,8 @@
 	<body>
 		<h1>Overworked Doctor</h1>
 		<p><strong>Joe Dough</strong> is a 30 year old Doctor, he's a health nut and he loves to buy local. He's very busy and rarely has time to make it to the farmers market. Joe is  always on his iphone throughout the day and uses a Macbook to surf the net at home</p>
+		<h2>Frustrations</h2>
+		<p>Joe is frustrated with his work situation and not being able to go to the farmers market. Joe is sick of shopping for food at Walmart.</p>
 		<h2>Goal</h2>
 		<p>Joe's goal in using Roots n' table farmers market app, is to buy fresh locally grown produce over the internet and schedule a pick up time that's convenient for him.</p>
 	</body>
