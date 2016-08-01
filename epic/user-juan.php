0aec27bc5d72e9b8a98a45f6ae636a143901f6c8
Raul Villareal <rvillarrcal@cnm.edu>
2016-08-01 16:42:25 -0600

    updated use case for Local Restaurant

---
+++ epic/user-juan.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title> Juan Chinampas </title>
+	</head>
+	<body>
+		<h1>Juan Chinampas MegaFarms</h1>
+		<h2>Name</h2>
+		<p>Juan Chinampas</p>
+		<h2>Age 64</h2>
+		<h2>Description</h2>
