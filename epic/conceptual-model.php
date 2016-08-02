1e7c82eb25edf04271764d0a11c7e05d6d34935b
Robert Engelbert <Robe32002@yahoo.com>
2016-08-02 16:16:22 -0600

    Added a conceptual model.

---
+++ epic/conceptual-model.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<title>Conceptual model</title>
+	</head>
+	<body>
+		<h1>Conceptual model</h1>
+		<ol>
+			<li><strong>One</strong> user can buy <strong>one</strong> product.</li>
+			<li><strong>One</strong> user can buy <strong>many</strong> products.</li>
+			<li><strong>Many</strong> users can buy <strong>one</strong> product.</li>
+			<li><strong>Many</strong> users can buy <strong>many</strong> products.</li>
+			<li><strong>one</strong> user can buy from <strong>many</strong> sellers</li>
+			<li><strong>Many</strong> users can buy from <strong>many</strong> sellers</li>
+		</ol>
+	</body>
+</html>
