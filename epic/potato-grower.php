013c9aa25e428ca070b2dbc2ddc7232a92ed3145
Robert Engelbert <Robe32002@yahoo.com>
2016-08-02 13:07:18 -0600

    Added an interaction flow

--- epic/potato-grower.php
+++ epic/potato-grower.php
@@ -13,5 +13,11 @@
 		<p>Maria's goal in using the Roots n' table farmers market app, is to sell her potato's and flowers online. Then she can fill orders at a time that's convenient for her, While earning an income from home. </p>
 		<h2>Use case</h2>
 		<p>Maria's motivation in using the Roots n' table farmers market app is to earn an income from home. Maria will grow potato's and flowers on the same city lot her house is located on utilizing both the front and backyard. Maria will use the Roots n' table farmers market app to sell her potato's and flower's. She will use her HP Notebook and LG K7 Android phone to manage her account and sell her products on the Roots n' table farmers market app.</p>
+		<h2>Interaction flow</h2>
+		<ol>
+			<li>Maria has got on to the Roots n' table app and logged in.</li>
+			<li>Maria has navigated to her Account page and checks her orders so she know's what to get ready for pickups</li>
+			<li>Maria uploads new images and product descriptions for her product's.</li>
+		</ol>
 	</body>
 </html>
