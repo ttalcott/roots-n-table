7facd81cfb14160f9519940c2c11c65f40ed18ee
Robert Engelbert <Robe32002@yahoo.com>
2016-08-01 18:38:37 -0600

    Added goals to the persona for Maria the potato grower.

--- epic/potato-grower.php
+++ epic/potato-grower.php
@@ -9,5 +9,7 @@
 		<p><strong>Maria Jones:</strong> a 36 year old gardener and stay at home mom that wants to earn an income. She uses a LG K7 Android phone and a HP Notebook laptop to access the internet. Maria will be using Roots n' table farmers market app to sell her Potato's and flower's</p>
 		<h2>Frustrations</h2>
 		<p>Maria is frustrated because she can't make it to the farmers market to sell her delicious potato's or beautiful flowers.</p>
+		<h2>Goal</h2>
+		<p>Maria's goal in using the Roots n' table farmers market app, is to sell her potato's and flowers online. Then she can fill orders at a time that's convenient for her, While earning an income from home. </p>
 	</body>
 </html>
