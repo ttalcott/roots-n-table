7840fb5f93462b470635e7a989fdaea8c5777706
Raul Villareal <rvillarrcal@cnm.edu>
2016-08-02 16:06:03 -0600

    added interaction flow pick up location method

--- epic/user-joan.php
+++ epic/user-joan.php
@@ -9,11 +9,42 @@
 		<h2>Persona</h2>
 		<p><strong> Name: </strong>Joan Blossoms</p>
 		<p><strong>Age: </strong> 56</p>
-		<p><strong>Occupation: </strong>Joan is a successful business person. She is the owner of "Joan Blossoms MegaFarms", a successful business the sells to a large retailer corporation. She has always been involved in the agricultural field. This is her passion and she has always dedicated to it with a huge sense of ethics. Joan's products are top quality and she takes proud on running her business. The technology she constantly uses for personal as well as busines purposes is her Samsung Galaxy S5 with an Android Kit Kat 4.4.4 operative System.</p>
-		<p><strong>Attitudes and Behaviors: </strong>Joan's business is very busy and she is a very detail oriented person, she likes to be involved as much as possible in the quality control process of her products. She likes to make sure the product is as close to perfect as possible, which is the reason her schedule is very full and complicated, and also the reason why she gets to sell her products to a large retailer and her products to be known across the nation. </p>
-		<p><strong>Needs and Frustrations: </strong>In recent years her client has pressured her to drop her products prices and Joan's income has experienced a constant drop. Since such downward spiral doesn't seem to end, she is determined to find more clients in order to diversify her sources of income. </p>
-		<p><strong>Goal: </strong>At this time, Joan is looking to develop new relationships with persons, stores, restaurants or any other entity that appreciates her products and is willing to pay a fair price for them.</p>
+		<p><strong>Occupation: </strong>Joan is a successful business person. She is the owner of "Joan Blossoms
+			MegaFarms", a successful business the sells to a large retailer corporation. She has always been involved in
+			the agricultural field. This is her passion and she has always dedicated to it with a huge sense of ethics.
+			Joan's products are top quality and she takes proud on running her business. The technology she constantly uses
+			for personal as well as busines purposes is her Samsung Galaxy S5 with an Android Kit Kat 4.4.4 operative
+			System.</p>
+		<p><strong>Attitudes and Behaviors: </strong>Joan's business is very busy and she is a very detail oriented
+			person, she likes to be involved as much as possible in the quality control process of her products. She likes
+			to make sure the product is as close to perfect as possible, which is the reason her schedule is very full and
+			complicated, and also the reason why she gets to sell her products to a large retailer and her products to be
+			known across the nation. </p>
+		<p><strong>Needs and Frustrations: </strong>In recent years her client has pressured her to drop her products
+			prices and Joan's income has experienced a constant drop. Since such downward spiral doesn't seem to end, she
+			is determined to find more clients in order to diversify her sources of income. </p>
+		<p><strong>Goal: </strong>At this time, Joan is looking to develop new relationships with persons, stores,
+			restaurants or any other entity that appreciates her products and is willing to pay a fair price for them.</p>
 		<h2>Use Case</h2>
-		<p> Joan has dedicated her life to produce high quality food and technology proficiency is not part of her skills set. She knows the internet can be helpful in looking for new customers and she is a regular social media user. Therefore, she has looked for farmers markets, local restaurants and neighbor associations websites and forums. Unfortunately she hasn't found a friendly, easy to use website that could help her break the above mentioned dependency in her main/only client without sacrificing other responsibilities and obligations. Additionally she doesn't have a laptop or desktop equipment and most of those websites are not very mobile friendly. Purchasing a new laptop or desktop would not represent a financial issue for her, but she would need to be trained in using them and she is far more comfortable with the idea of using her Samsung Galaxy S5 with an Android Kit Kat 4.4.4 operative System for business purposes.</p>
+		<p> Joan has dedicated her life to produce high quality food and technology proficiency is not part of her skills
+			set. She knows the internet can be helpful in looking for new customers and she is a regular social media user.
+			Therefore, she has looked for farmers markets, local restaurants and neighbor associations websites and forums.
+			Unfortunately she hasn't found a friendly, easy to use website that could help her break the above mentioned
+			dependency in her main/only client without sacrificing other responsibilities and obligations. Additionally she
+			doesn't have a laptop or desktop equipment and most of those websites are not very mobile friendly. Purchasing
+			a new laptop or desktop would not represent a financial issue for her, but she would need to be trained in
+			using them and she is far more comfortable with the idea of using her Samsung Galaxy S5 with an Android Kit Kat
+			4.4.4 operative System for business purposes.</p>
 	</body>
+	<h3>Interaction Flow</h3>
+	<ol>
+		<li>Joan gets to roots 'n table website and signs in to access her account.</li>
+		<li>Joan goes to her profile/store page to upload her available products.</li>
+		<li>Afterwards she adds all related information about new item (description, amount, picture, price).</li>
+		<li>Joan clicks on upload button after entering all data about the products she wants to put on the site.</li>
+		<li>Joan goes back to her account page to see if anyone has bought anything and if she needs to get orders ready.</li>
+		<li>Joan looks at the pending orders page and gets her employees to get them ready.</li>
+		<li>Since she saved a lot of time, she takes the time to explore the site and see if she wants to buy something.</li>
+		<li>She buys a couple of solid chicken and two dozens of liquid chickens (eggs) and terminates her session.</li>
+	</ol>
 </html>
