d787bc7293c2094ca7e267f8891f960e415dc7a4
Raul Villareal <rvillarrcal@cnm.edu>
2016-08-02 17:05:14 -0600

    added corrections, but still need some clarifying

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -7,17 +7,17 @@
 	<body>
 		<h1>Conceptual model</h1>
 		<ol>
-			<li><strong>One</strong> user can buy <strong>one</strong> product.</li>
-			<li><strong>One</strong> user can buy <strong>many</strong> products.</li>
-			<li><strong>Many</strong> users can buy <strong>one</strong> product.</li>
-			<li><strong>Many</strong> users can buy <strong>many</strong> products.</li>
-			<li><strong>one</strong> user can buy from <strong>many</strong> sellers</li>
-			<li><strong>Many</strong> users can buy from <strong>many</strong> sellers</li>
-			<li><strong>One</strong> grower can have <strong>many</strong> products.</li>
-			<li><strong>One</strong> grower can have <strong>many</strong> customers.</li>
-			<li><strong>Many</strong> growers can have <strong>many</strong> products.</li>
-			<li><strong>Many</strong> growers can have <strong>many</strong> customers</li>
-			<li><strong>One</strong> grower can buy <strong>many</strong> products</li>
+<!--			<li><strong>One</strong> users can buy <strong>many</strong> products.</li>-->
+<!--			<li><strong>Many</strong> users can buy from <strong>many</strong> sellers</li>-->
+<!--			<li><strong>One</strong> grower can have <strong>many</strong> products.</li>-->
+<!--			<li><strong>One</strong> grower can have <strong>many</strong> customers.</li>-->
+<!--			<li><strong>Many</strong> growers can have <strong>many</strong> products.</li>-->
+<!--			<li><strong>Many</strong> growers can have <strong>many</strong> customers</li>-->
+<!--			<li><strong>One</strong> grower can buy <strong>many</strong> products</li>-->
+			<li><strong>One</strong> user can own <strong>many</strong> products</li>
+			<li><strong>One</strong> user can create <strong>many</strong> orders</li>
+			<li><strong>Many</strong> orders can contain <strong>many</strong> products</li>
+			<li><strong>Many</strong>products can be sorted in <strong>one</strong> category</li>
 		</ol>
 	</body>
 </html>
