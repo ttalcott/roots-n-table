930c643077fd994e32616a2f0d8498a4b014b7f2
Robert Engelbert <Robe32002@yahoo.com>
2016-08-02 15:18:01 -0600

    Added the conceptual modal user joe.

--- epic/user-joe.php
+++ epic/user-joe.php
@@ -20,5 +20,14 @@
 			<li>Joe selects the items he wants to buy.</li>
 			<li>Joe checks out and schedules a convenient time to pick up his produce.</li>
 		</ol>
+		<h2>Conceptual modal</h2>
+		<ol>
+			<li><strong>One</strong> user can buy <strong>one</strong> product.</li>
+			<li><strong>One</strong> user can buy <strong>many</strong> products.</li>
+			<li><strong>Many</strong> users can buy <strong>one</strong> product.</li>
+			<li><strong>Many</strong> users can buy <strong>many</strong> products.</li>
+			<li><strong>one</strong> user can buy from <strong>many</strong> sellers</li>
+			<li><strong>Many</strong> users can buy from <strong>many</strong> sellers</li>
+		</ol>
 	</body>
 </html>
