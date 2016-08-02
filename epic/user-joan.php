0dbcd24ce452a081c06ea3dd620d267d49f7859d
Raul Villareal <rvillarrcal@cnm.edu>
2016-08-01 21:08:12 -0600

    added user-joan persona

--- epic/user-joan.php
+++ epic/user-joan.php
@@ -10,10 +10,10 @@
 		<p>Joan Blossoms</p>
 		<h2>Age 56</h2>
 		<h2>Description</h2>
-		<p>Joan is a successful business person. She has always been involved in the agricultural field. This is her passion and she has always dedicated to it with a huge sense of ethics. Joan's products are top quality and she takes proud on running her business. Her business is very busy and she sells product to one large retailer, who sells them accross the nation. Nevertheless, in recent years the prices of her products have experienced a constant drop. Since her main client has forced her to accept such terms and her profit margin has decreased too, she is determine to find supplementary and/or different sources of income (more clients) </p>
+		<p>Joan is a successful business person. She has always been involved in the agricultural field. This is her passion and she has always dedicated to it with a huge sense of ethics. Joan's products are top quality and she takes proud on running her business. Her business is very busy and she sells product to one large retailer, who sells them accross the nation. Nevertheless, in recent years her client has pressured her to drop her products prices and Joan's products have experienced a constant drop. Since such down spiral doesn't seem to end, she is determine to find more clients in order to diversify her sources of income. </p>
 		<h2>Goal</h2>
-		<p></p>
+		<p>At this time, Joan is looking to develop new relationships with persons, stores, restaurants or any other entity that appreciates her products and pys a fair price for them.</p>
 		<h2>Use Case</h2>
-		<p></p>
+		<p> Joan has dedicated her life to produce high quality food and technology proficiency is not part of her skills set. She knows the internet can be helpful in looking for new customers and she is a regular social media user. She doesn't have a good laptop or desktop equipment. Purchasing one should not represent a problem, but she would need to be trained in using them and she is far more comfortable with the idea of using her Samsung Galaxy S5 with an Android Kit Kat 4.4.4 operative System for business purposes.</p>
 	</body>
 </html>
