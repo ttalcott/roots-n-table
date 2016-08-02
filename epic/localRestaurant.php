5bb91409d1c68f6d9393fed5f4c64dc8ba791cff
Raul Villareal <rvillarrcal@cnm.edu>
2016-08-02 13:29:39 -0600

    modify and ready for review user local restaurant persona and use case

--- epic/localRestaurant.php
+++ epic/localRestaurant.php
@@ -1,34 +1,37 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" xmlns="http://www.w3.org/1999/html">
 	<head>
 		<meta charset="UTF-8"/>
 		<title> Local Rest </title>
 	</head>
 	<body>
 		<h1> Local Restaurant Owner </h1>
-		<h2> Name</h2>
-		<p> Benjamin Cook </p>
-		<h2> Age 29 </h2>
-		<h2> Description </h2>
-		<p> Benjamin is a person that has always liked good food. His taste has driven him to start a small business in
-			the food industry. His restaurant started operations 5 years ago and ever since, it has experienced a rapid
-			growth. Benjamin has decided from the beginning to use only the best ingredients for his recipes. Such, is the
-			main reason why people visits his establishment, but it is also the main struggle for Benjamin because high
-			quality and highly fresh ingredients are hard to find. </p>
-		<h2> Frustrations</h2>
-		<p>Benjamin is constantly looking for vegetable and animal products providers to satisfy the needs of his
-			business, but lately, he's experiencing an increment on his business operations and his regular providers are
-			unable to meet his demands. Therefore, he needs to find new sources for such ingredients and he suddenly is
-			finding himself with less time to look for them.</p>
-		<h2> Goal </h2>
-		<p> At this moment, Benjamin needs to find a bigger amount of the high quality ingredients he uses for his recipes
-			and do it in a smaller amount of time. </p>
+		<h2>Persona</h2>
+		<p><strong>Name:</strong>Benjamin Cook </p>
+		<h2><strong>Age: </strong> 29 </h2>
+		<p><strong>Occupation: </strong> Benjamin is a person that has always liked good food. His taste has driven him
+			to start a small business in the food industry. His restaurant started operations 5 years ago and ever since,
+			it has experienced a rapid growth. Benjamin has decided from the beginning to use only the best ingredients for
+			his recipes, which is the main reason why people visits his establishment.</p>
+		<p><strong> Attitudes and Behaviors: </strong> Benjamin takes his business very seriously and likes to be involved
+			in all stages of the process. He supervises everything, specially when it comes to his ingredients chain of
+			supply. Nevertheless, he has already built trusting relationships with his suppliers through these years so he
+			doesn't have to spend a lot of time overseeing the ingredients quality. </p>
+		<p><strong> Needs and Frustrations: </strong>Benjamin is constantly looking for vegetable and animal products
+			providers to satisfy the needs of his business, but lately, he's experiencing an increment on his business
+			operations and his regular providers are unable to meet his demands. Therefore, he needs to find new sources
+			for such ingredients and he suddenly is finding himself with less time to look for them. Currently, in order
+			for him to satisfy his clients he had compromised quality in exchange to availability. Currently he needs to go
+			to retail grocery stores (Smith's, Albertson's, Walmart) in order to make up for his needs; he feels disgusted
+			by this method, but it helps him satify his day by day operations.</p>
+		<p><strong> Goal: </strong> At this moment, Benjamin needs to find a bigger amount of the high quality ingredients
+			he uses for his recipes and do it in a smaller amount of time. </p>
 		<h2>Use Case</h2>
 		<p>Benjamin needs to find a way in which he can optimize his daily operations. Something that gives him access to
 			vegetable and animal products in a way that doesn't take a lot of time. A tool where he can quickly learn about
 			the product in a way that he can make an informed decision about purchasing. Finally, he needs to develop long
 			term trust relationships with his new providers. Benjamin offers wi-fi to his customers and that
-			means he has a very reliable and powerful connection. In addition he has a powerful MacBook Pro with OS X (10.11.6) "El
-			Capitan" and an Iphone 6 with IOS 9.3.3 operative system.</p>
+			means he has a very reliable and powerful connection. In addition he has a powerful MacBook Pro with OS X
+			(10.11.6) "El Capitan" and an Iphone 6 with IOS 9.3.3 operative system.</p>
 	</body>
 </html>
