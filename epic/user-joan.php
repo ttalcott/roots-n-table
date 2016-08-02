e2af64c88d421427e250cbff458a765e3d06ac42
Raul Villareal <rvillarrcal@cnm.edu>
2016-08-02 13:05:30 -0600

    finished Joan persona and use case (for review)

--- epic/user-joan.php
+++ epic/user-joan.php
@@ -6,14 +6,14 @@
 	</head>
 	<body>
 		<h1>Joan Blossoms MegaFarms</h1>
-		<h2>Name</h2>
-		<p>Joan Blossoms</p>
-		<h2>Age 56</h2>
-		<h2>Description</h2>
-		<p>Joan is a successful business person. She has always been involved in the agricultural field. This is her passion and she has always dedicated to it with a huge sense of ethics. Joan's products are top quality and she takes proud on running her business. Her business is very busy and she sells product to one large retailer, who sells them accross the nation. Nevertheless, in recent years her client has pressured her to drop her products prices and Joan's products have experienced a constant drop. Since such down spiral doesn't seem to end, she is determine to find more clients in order to diversify her sources of income. </p>
-		<h2>Goal</h2>
-		<p>At this time, Joan is looking to develop new relationships with persons, stores, restaurants or any other entity that appreciates her products and pys a fair price for them.</p>
+		<h2>Persona</h2>
+		<p><strong> Name:</strong>Joan Blossoms</p>
+		<p><strong>Age:</strong> 56</p>
+		<p><strong>Occupation</strong>Joan is a successful business person. She is the owner of "Joan Blosoms MegaFarms", a succesful busines the sells to a large retailer corporation. She has always been involved in the agricultural field. This is her passion and she has always dedicated to it with a huge sense of ethics. Joan's products are top quality and she takes proud on running her business.</p>
+		<p><strong>Attitudes and Behaviors:</strong>Joan's business is very busy and she is a very detail oriented person, she likes to be involved as much as possible in the quality control process of her products. She likes to make sure the product is as close to perfect as possible, which is the reason her schedule is very full and complicated, and also the reason why she gets to sell her products to a large retailer and her products to be known accross the nation. </p>
+		<p><strong>Needs and Frustrations:</strong>In recent years her client has pressured her to drop her products prices and Joan's income has experienced a constant drop. Since such downward spiral doesn't seem to end, she is determined to find more clients in order to diversify her sources of income. </p>
+		<p><strong>Goal</strong>At this time, Joan is looking to develop new relationships with persons, stores, restaurants or any other entity that appreciates her products and is willing to pay a fair price for them.</p>
 		<h2>Use Case</h2>
-		<p> Joan has dedicated her life to produce high quality food and technology proficiency is not part of her skills set. She knows the internet can be helpful in looking for new customers and she is a regular social media user. She doesn't have a good laptop or desktop equipment. Purchasing one should not represent a problem, but she would need to be trained in using them and she is far more comfortable with the idea of using her Samsung Galaxy S5 with an Android Kit Kat 4.4.4 operative System for business purposes.</p>
+		<p> Joan has dedicated her life to produce high quality food and technology proficiency is not part of her skills set. She knows the internet can be helpful in looking for new customers and she is a regular social media user. Therefore, she has looked for farmers markets, local restaurants and neighbor associations websites and forums. Unfortunately she hasn't found a friendly, easy to use website that could help her break the above mentioned dependency in her main/only client without sacrificing other responsabilities and obligations. Additionally she doesn't have a good laptop or desktop equipment and most of those websites are not very mobile friendly. Purchasing a new laptop or desktop would not represent a financial issue for her, but she would need to be trained in using them and she is far more comfortable with the idea of using her Samsung Galaxy S5 with an Android Kit Kat 4.4.4 operative System for business purposes.</p>
 	</body>
 </html>
