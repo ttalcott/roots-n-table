278db26e7dc92db0726e7ce25422f03053bcea41
Travis Talcott <ttalcott@aquilagroup.com>
2016-08-02 13:56:26 -0600

    added persona to farmers market frequent flyer

--- epic/frequent-flyer.php
+++ epic/frequent-flyer.php
@@ -10,6 +10,15 @@
 				<h1>Erica: Farmers Market Frequent Flyer</h1>
 			</header>
 		</div>
-		
+		<div class="persona">
+			<h3>Persona:</h3>
+			<ul>
+				<li>Age: 57 years old</li>
+				<li>Occupation: Used to be a marketing wizard but now she enjoys volunteering her time to good causes.</li>
+				<li>Goals: Erica is a true farmers market frequent flyer: she goes to many markets every week. However, she always runs into the problem that many of her favorite products are sold out by the time she gets there. She hopes to find a solution that allows her to reserve and pay for her favorites ahead of time so that she can have a guarantee that she will get what she wants. She also hopes that the site is easy to use because, well, she doesn't technology.</li>
+				<li>Needs from an online farmers market (Roots 'n Table): Erica needs for the UI to be easy to navigate. She doesn't have a good sense for technology so she doesn't want to spend an hour trying to figure out what to do.</li>
+				<li>Relevant technology: Erica has a computer but couldn't tell you what the brand was or the operating system (it's a Dell Inspiron that runs Windows 7). She also has an iPhone 6 but does most of her browsing/online shopping from home on her laptop.</li>
+			</ul>
+		</div>
 	</body>
 </html>
