d0e6f60e8dad1631c48b47f258fc3d8749015fe6
Travis Talcott <ttalcott@aquilagroup.com>
2016-08-05 08:44:53 -0600

    created profile table in mySQL

---
+++ sql/roots-n-table.sql
@@ -0,0 +1,20 @@
+
+CREATE TABLE profile (
+	profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	profileActivationToken CHAR(28),
+	profileEmail VARCHAR(128) NOT NULL,
+	profileFirstName VARCHAR (16) NOT NULL,
+	profileHash CHAR(128) NOT NULL,
+	profileLastName VARCHAR(16) NOT NULL,
+	profilePhoneNumber VARCHAR(32) NULL,
+	profileSalt CHAR(64) NOT NULL,
+	profileType VARCHAR(8) NOT NULL,
+	profileUserName VARCHAR(32) NOT NULL,
+	UNIQUE(profileActivationToken),
+	UNIQUE(profileEmail),
+	UNIQUE(profileHash),
+	UNIQUE(profileSalt),
+	UNIQUE(profileUserName),
+	PRIMARY KEY(profileId)
+) ;
+
