08555cf4bf3136f05d18d686687639854e12d937
Robert Engelbert <Robe32002@yahoo.com>
2016-08-06 15:45:05 -0600

    Renamed two files to productTest and rootsTableTest. https://app.asana.com/0/161555263951790/164161356807354

--- public_html/test/productTest.php
+++ public_html/test/productTest.php
@@ -2,7 +2,7 @@
 namespace Edu\Cnm\rootstable\Test;
 
 //grab the project test parameters
-require_once("roots-table-test.php");
+require_once("rootsTableTest.php");
 
 //grab the class under scrutiny
 require_once(dirname(__DIR__) . "/public_html/php/classes/autoload.php");
