9b00e575bddeecc8ff36c167007dbcb90da55a28
Robert Engelbert <Robe32002@yahoo.com>
2016-08-06 10:44:47 -0600

    Added function to product test. https://app.asana.com/0/161555263951790/164161356807354

--- public_html/test/product.php
+++ public_html/test/product.php
@@ -1,5 +1,7 @@
 <?php
 
 abstract class ProductTest extends \PHPUnit_Extensions_rootstable_TestCase{
-	
+	const INVALID_KEY = 4294967296;
+	protected $connection = null;
+	public final function getDataSet(){}
 }
