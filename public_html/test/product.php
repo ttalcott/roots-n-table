55dfd9982b73a8c7b2da4830158155983da18255
Robert Engelbert <Robe32002@yahoo.com>
2016-08-06 12:51:42 -0600

    Added test for product class. https://app.asana.com/0/161555263951790/164161356807354

---
+++ public_html/test/product.php
@@ -0,0 +1,23 @@
+<?php
+namespace Edu\Cnm\rootstable\Test;
+
+//grab the project test parameters
+require_once("roots-table-test.php");
+
+//grab the class under scrutiny
+require_once(dirname(__DIR__) . "/public_html/php/classes/autoload.php");
+
+/**
+ * Full PHPUnit test for the Product class
+ *
+ * This is a complete PHPUnit test of the Product class. It is complete becase *ALL* mySQL/PDO enabled methods are tested for both invalid and valid inputs.
+ *
+ * @see ProductTest
+ * @author Robert Engelbert <rengelbert@cnm.edu>
+ */
+class ProductTest extends rootstable {
+	/**
+	 * content of the product
+	 * @var 
+	 */
+}
