8d0136f7b6c6ef271abb45c5afcd87ab24ad203d
Raul Villareal <rvillarrcal@cnm.edu>
2016-08-07 15:09:55 -0600

    added the remainder 7 tests

--- public_html/test/locationTest.php
+++ public_html/test/locationTest.php
@@ -17,16 +17,63 @@ require_once(dirname(__DIR__) . "/public_html/php/classes/autoload.php");
 
 class LocationTest extends rootstableTest {
 	/**
-	 * Let's start with the content of the Location class
+	 * Let's start with the content of the locationId
 	 * This is the primary key
 	 * @var int $locationId
 	 */
 	//Not sure if this is correct I just copied Robert
 	public $locationId = "YOU'RE NULL Zero, Zip, Nothing, Nada, Ni Maizz";
+
 	/**
 	 * content of the locationProfileId
 	 * @var int $locationProfileId
 	 */
 	public $locationProfileId = "Fuzzy to the second power?";
+
+	/**
+	 * content of the locationName
+	 * @var int $locationName
+	 */
+	public $locationName = "What is ur place's name?";
+
+	/**
+	 * content of the locationAttention
+	 * @var int $locationAttention
+	 */
+	public $locationAttention = "Who's the gatekeeper?";
+
+	/**
+	 * content of the locationStreetOne
+	 * @var int $locationStreetOne
+	 */
+	public $locationStreetOne = "Where's your farm at?";
+
+	/**
+	 * content of the locationStreetTwo
+	 * @var int $locationStreetTwo
+	 */
+	public $locationStreetTwo = "I need more details?";
+
+	/**
+	 * content of the locationCity
+	 * @var int $locationCity
+	 */
+	public $locationCity = "Is it ABQ?";
+
+	/**
+	 * content of the locationState
+	 * @var int $locationState
+	 */
+	public $locationState = "Is it in the Land of Enchantment?";
+
+	/**
+	 * content of the locationZipCode
+	 * @var int $locationZipCode
+	 */
+	public $locationZipCode = "Gimmy 5... digits";
+
+
+
+
 
 }
