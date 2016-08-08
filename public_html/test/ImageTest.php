d2548645728594b82116b704394595135a4a1b3c
Robert Engelbert <Robe32002@yahoo.com>
2016-08-08 14:24:03 -0600

    I don't understand writing the test for ImageTest.php first so I'm putting this on hold and going to try the class first. https://app.asana.com/0/161555263951790/164161356807360

--- public_html/test/ImageTest.php
+++ public_html/test/ImageTest.php
@@ -6,3 +6,30 @@ require_once("rootsTableTest.php");
 
 //grab the class under scrutiny
 require_once(dirname(__DIR__) . "/public_html/php/classes/autoload.php");
+
+/**
+ * @see Image 
+ * @author Robert Engelbert <rengelbert@cnm.edu>
+ */
+
+class ImageTest extends rootsTableTest{
+	/**
+	 * @var Int $VALID_IMAGEID
+	 */
+	protected $VALID_IMAGEID = NULL;
+	/**
+	 * @var Int $VALID_IMAGEPATH
+	 */
+	protected $VALID_IMAGEPATH = NULL;
+
+	/**
+	 * @var Int $VALID_IMAGETYPE
+	 */
+	protected $VALID_IMAGETYPE = NULL;
+
+	public final function setUp(){
+		//run default setUp() method first
+		parent::setUp();
+		
+	}
+}
