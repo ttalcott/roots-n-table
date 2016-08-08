b78eed12af19fb4ae119a8e62b680477a6e2d341
Robert Engelbert <Robe32002@yahoo.com>
2016-08-08 13:53:07 -0600

    started ImageTest.php

---
+++ public_html/test/ImageTest.php
@@ -0,0 +1,8 @@
+<?php
+namespace Edu\Cnm\rootstable\Test;
+
+//grab the project test parameters
+require_once("rootsTableTest.php");
+
+//grab the class under scrutiny
+require_once(dirname(__DIR__) . "/public_html/php/classes/autoload.php");
