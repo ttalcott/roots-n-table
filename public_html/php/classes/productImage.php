6c5823dda9e63c98a3a74c1cf36e01f1793eb56a
Robert Engelbert <Robe32002@yahoo.com>
2016-08-09 15:40:05 -0600

    Added productImageImageId accessor and mutator. https://app.asana.com/0/161555263951790/164161356807367

--- public_html/php/classes/productImage.php
+++ public_html/php/classes/productImage.php
@@ -1,1 +1,35 @@
 <?php
+
+/**
+ * Class ProductImage
+ */
+class ProductImage{
+	private $productImageImageId;
+	private $productImageProductId;
+
+	/**
+	 * Accessor method for productImageImageId
+	 *
+	 * @return mixed
+	 */
+	public function getProductImageImageId(){
+		return($this->productImageImageId);
+	}
+
+	/**
+	 * Mutator method for ProductImageImageId
+	 *
+	 * @param int $newProductImageImageId
+	 * @throws RangeException if value is not apositive number
+	 */
+	public function setProductImageImageId(int $newProductImageImageId){
+		//filter productImageImageId
+		$productImageImageId = filter_var($newProductImageImageId);
+		//check to see if value is greater than 0
+		if($newProductImageImageId <= 0){
+			throw(new \RangeException("This must be a positive number"));
+		}
+		// convert and store the value
+		$this->productImageImageId = intval($newProductImageImageId);
+	}
+}
