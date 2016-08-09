4b776ae25f954b019687f2b9a85a4ebff207c799
Robert Engelbert <Robe32002@yahoo.com>
2016-08-09 16:04:18 -0600

    Added productImageProductId accessor and mutator and a ProductImage constructor. https://app.asana.com/0/161555263951790/164161356807367

--- public_html/php/classes/productImage.php
+++ public_html/php/classes/productImage.php
@@ -7,29 +7,73 @@ class ProductImage{
 	private $productImageImageId;
 	private $productImageProductId;
 
-	/**
-	 * Accessor method for productImageImageId
-	 *
-	 * @return mixed
-	 */
-	public function getProductImageImageId(){
-		return($this->productImageImageId);
+	public function __construct($newProductImageImageId,$newProductImageProductId =null) {
+		try {
+			$this->productImageImageId = ($newProductImageImageId);
+			$this->productImageProductId = ($newProductImageProductId);
+		} catch(\RangeException $range) {
+			//rethrow exception
+			throw(new \RangeException($range->getMessage(), 0, $range));
+		} catch(\Exception $exception) {
+			//rethrow exception
+			throw(new \Exception($exception->getMessage(), 0, $exception));
+		}
 	}
 
-	/**
-	 * Mutator method for ProductImageImageId
-	 *
-	 * @param int $newProductImageImageId
-	 * @throws RangeException if value is not apositive number
-	 */
-	public function setProductImageImageId(int $newProductImageImageId){
-		//filter productImageImageId
-		$productImageImageId = filter_var($newProductImageImageId);
-		//check to see if value is greater than 0
-		if($newProductImageImageId <= 0){
-			throw(new \RangeException("This must be a positive number"));
+
+		/**
+		 * Accessor method for productImageImageId
+		 *
+		 * @return mixed
+		 */
+		public
+		function getProductImageImageId() {
+			return ($this->productImageImageId);
 		}
-		// convert and store the value
-		$this->productImageImageId = intval($newProductImageImageId);
+
+		/**
+		 * Mutator method for ProductImageImageId
+		 *
+		 * @param int $newProductImageImageId
+		 * @throws RangeException if value is not a positive number
+		 */
+		public
+		function setProductImageImageId(int $newProductImageImageId) {
+			//filter productImageImageId
+			$productImageImageId = filter_var($newProductImageImageId);
+			//check to see if value is greater than 0
+			if($newProductImageImageId <= 0) {
+				throw(new \RangeException("This must be a positive number"));
+			}
+			// convert and store the value
+			$this->productImageImageId = intval($newProductImageImageId);
+		}
+
+		/**
+		 * Accessor method for productImageProductId
+		 *
+		 * @return mixed
+		 */
+		public
+		function getProductImageProductId() {
+			return ($this->productImageProductId);
+		}
+
+		/**
+		 * Mutator method for productImageProductId
+		 *
+		 * @param $newProductImageProductId
+		 * @throws RangeException if value is not a positive number
+		 */
+		public
+		function setProductImageProductId($newProductImageProductId) {
+			//filter productImageProductId
+			$productImageProductId = filter_var($newProductImageProductId);
+			//check to see if value is greater than 0
+			if($newProductImageProductId <= 0) {
+				throw(new \RangeException("This must be a positive number"));
+			}
+			// convert and store the value
+			$this->productImageProductId = intval($newProductImageProductId);
+		}
 	}
-}
