8e8534680ef93b6fb86d33700b36c7d7c36f3b2b
Raul Villareal <rvillarrcal@cnm.edu>
2016-08-10 21:49:01 -0600

    added json serializable to ProductPurchase https://app.asana.com/0/161549255700593/164161356807365

--- public_html/php/classes/ProductPurchase.php
+++ public_html/php/classes/ProductPurchase.php
@@ -348,5 +348,15 @@ class ProductPurchase implements \JsonSerializable {
 		}
 		return($productPurchase);
 	}
+
+	/**
+	 * formats the state variables for JSON serialization
+	 *
+	 * @return array resulting state variables to serialize
+	 **/
+	public function jsonSerialize() {
+		$fields = get_object_vars($this);
+		return($fields);
+	}
 }
 ?>
