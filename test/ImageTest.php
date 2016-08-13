2585de204823ce66aa6238badc5aa0243df70812
Robert Engelbert <Robe32002@yahoo.com>
2016-08-12 19:49:52 -0600

    Added testGetInvalidImageByType to ImageTest.php https://app.asana.com/0/161549255700604/164161356807360

--- test/ImageTest.php
+++ test/ImageTest.php
@@ -14,10 +14,6 @@ require_once(dirname(__DIR__) . "/public_html/php/classes/autoload.php");
 
 class ImageTest extends rootsTableTest{
 	/**
-	 * @var Int $VALID_IMAGEID
-	 */
-	protected $VALID_IMAGEID = NULL;
-	/**
 	 * @var Int $VALID_IMAGEPATH
 	 */
 	protected $VALID_IMAGEPATH = NULL;
@@ -139,6 +135,55 @@ class ImageTest extends rootsTableTest{
 		//grab an id that exceeds the maximum allowable value
 		$image = Image::getImageByImageId($this->getPDO(), RootsTableTest::INVALID_KEY);
 		$this->assertNull($image);
+	}
+	/**
+	 * test grabbing an image by path
+	 */
+	public function testGetValidImageByPath(){
+		//count the number of rows currently in the database
+		$numRows = $this->getConnection()->getRowCount("Image");
+
+		//create a new image and insert into mySQL
+		$image = new Image(null, $this->VALID_IMAGEPATH, $this->VALID_IMAGETYPE);
+		$image->insert($this->getPDO());
+
+		//grab data from mySQL and enforce that the fields match
+		$pdoImage = Image::getImageByImagePath($this->getPDO(), $this->VALID_IMAGEPATH);
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("image"));
+		$this->assertEquals($pdoImage[0]->getImagePath(), $this->VALID_IMAGEPATH);
+		$this->assertEquals($pdoImage[0]->getImageType(), $this->VALID_IMAGETYPE);
+	}
+	/**
+	 *test for grabbing an image by path that does not exist
+	 */
+	public function testGetInvalidImageByPath(){
+		$image = Image::getImageByImagePath($this->getPDO(),"coolPic");
+		$this->assertEquals($image->getSize(),0);
+	}
+	/**
+	 * test grabbing an image by type
+	 */
+	public function testGetValidImageByType(){
+		//count the number of rows currently in the database
+		$numRows = $this->getConnection()->getRowCount("image");
+
+		//create a new image and insert into mySQL
+		$image = new Image(null, $this->VALID_IMAGEPATH, $this->VALID_IMAGETYPE);
+		$image->insert($this->getPDO());
+
+		//grab data from mySQL and enforce that the fields match
+		$pdoImage = Image::getImageByImageType($this->getPdo(), $this->VALID_IMAGETYPE);
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("image"));
+		$this->assertEquals($pdoImage[0]->getImagePath(), $this->VALID_IMAGEPATH);
+		$this->assertEquals($pdoImage[0]->getImageType(),
+			$this->VALID_IMAGETYPE);
+	}
+	/**
+	 * test for grabbing an image by type that does not exsit
+	 */
+	public function testGetInvalidImageByType(){
+		$image = Image::getImageByImageType($this->getPDO(), "gif");
+		$this->assertEquals($image->getSize(),0);
 	}
 
 }
