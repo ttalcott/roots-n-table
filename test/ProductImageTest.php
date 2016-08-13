89683bebfb180355f2812b42c13495952dd64dfb
Robert Engelbert <Robe32002@yahoo.com>
2016-08-13 15:03:42 -0600

    Added testInsertValidProductImage ProductImageTest.php https://app.asana.com/0/161549255700604/164161356807367

--- test/ProductImageTest.php
+++ test/ProductImageTest.php
@@ -8,3 +8,42 @@ require_once ("RootsTableTest.php");
 
 //grab the class under scrutiny
 require_once (dirname(__DIR__) . "public_html/php/classes/autoload.php");
+/**
+ * PHPUnit test for ProducImage.php
+ * 
+ * @see ProductIamge
+ * @author Robert Engelbert <rengelbert@cnm.edu>
+ */
+class ProductImageTest extends RootsTableTest{
+	/**
+	 * @var $CATIMAGEIMAGEID;
+	 */
+	protected $CATIMAGEIMAGEID;
+	/**
+	 * @var  $CATIMAGEPRODUCTID;
+	 */
+	protected  $CATIMAGEPRODUCTID;
+	/**
+	 * test inserting a valid proudctImage and verify that the mySQL data matches
+	 */
+	public function testInsertValidProductImage(){
+		//get the count of the number of rows in the database
+		$numRows = $this->getConnection()->getRowCount("productImage");
+
+		//create a new productImage and insert into mySQL
+		$productImage = new ProductImage(null, $this->CATIMAGEIMAGEID, $this->CATIMAGEPRODUCTID);
+		$productImage->insert($this->getPDO());
+
+		//grab data from mySQL and ensure it matches
+		$pdoProductImage = ProductImage::getProductImageByProductImageImageId($this->getPDO(), $productImage->getProductImageImageId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("productImage"));
+		$this->assertEquals($pdoProductImage->getProductImageImageId(), $this->CATIMAGEIMAGEID);
+		$this->assertEquals($pdoProductImage->getProductImageProducId(), $this->CATIMAGEPRODUCTID);
+	}
+	/**
+	 * test inserting a productImage that cannot be added
+	 *
+	 * @expectedException \PDOException
+	 */
+	
+}
