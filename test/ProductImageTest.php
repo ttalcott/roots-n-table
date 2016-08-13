6d248b5a614ac296d521e2b30c578defc70c9c27
Robert Engelbert <Robe32002@yahoo.com>
2016-08-13 14:44:04 -0600

    Added a namespace and require once for ProductImageTest.php https://app.asana.com/0/161549255700604/164161356807367

---
+++ test/ProductImageTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace Edu\Cnm\Rootstable\Test;
+
+use Edu\Cnm\Rootstable\ProductImage;
+
+//grab the project parameters
+require_once ("RootsTableTest.php");
+
+//grab the class under scrutiny
+require_once (dirname(__DIR__) . "public_html/php/classes/autoload.php");
