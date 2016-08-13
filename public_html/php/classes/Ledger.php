300d9ca20c16a9ec1c1047289ea4cf1c6bf32fb2
Travis Talcott <ttalcott@aquilagroup.com>
2016-08-12 18:36:30 -0600

    added ValidateDate file to classes directory https://app.asana.com/0/161549255700582/164161356807352

---
+++ public_html/php/classes/Ledger.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Edu\Cnm\Rootstable;
+
+require_once("autoload.php");
+
+/**
+* Ledger class for Roots 'n Table
+*
+* @author Travis Talcott <ttalcott@lyradevelopment.com>
+* version 1.0.0
+**/
+
+class Ledger {
+	/**
+	* id for this ledger
+	* @var int $ledgerId
+	**/
+	private $ledgerId;
+}
+
+ ?>
