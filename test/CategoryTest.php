a19cd3a658c4fb757e0fb0f3440f0ea42b110868
Robert Engelbert <Robe32002@yahoo.com>
2016-08-13 09:07:25 -0600

    Added namespace and require_once for CategoryTest https://app.asana.com/0/161549255700604/164161356807356

---
+++ test/CategoryTest.php
@@ -0,0 +1,8 @@
+<?php
+namespace Edu\Cnm\Rootstable\Test;
+
+//grab the project test parameters
+require_once("RootsTableTest.php");
+
+//grab the class under scrutiny
+require_once(dirname(__DIR__) . "/public_html/php/classes/autoload.php");
