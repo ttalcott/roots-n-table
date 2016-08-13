9f3293d1c75c43cf3b26aac166e3f6119f80da3f
Travis Talcott <ttalcott@aquilagroup.com>
2016-08-12 18:43:24 -0600

    finished defining state variables https://app.asana.com/0/161549255700582/164161356807352

--- public_html/php/classes/Ledger.php
+++ public_html/php/classes/Ledger.php
@@ -12,11 +12,44 @@ require_once("autoload.php");
 **/
 
 class Ledger {
+	//load the ValidateDate file
+	use ValidateDate;
+
 	/**
 	* id for this ledger
 	* @var int $ledgerId
 	**/
 	private $ledgerId;
+	/**
+	* id of the purchase this ledger belongs to; this is the foreign key
+	* @var int $ledgerPurchaseId
+	**/
+	private $ledgerPurchaseId;
+	/**
+	* amount of this ledger
+	* @var decimal $ledgerAmount
+	**/
+	private $ledgerAmount;
+	/**
+	* date and time this ledger was created; in a php DateTime object
+	* @var \DateTime $ledgerDate
+	**/
+	private $ledgerDate;
+	 /**
+	 * stripe token for this ledger
+	 * @var string $ledgerStripeToken
+	 **/
+	 private $ledgerStripeToken;
+
+	 /**
+	 * accessor method for $ledgerId
+	 *
+	 * @return int|null value of $ledgerId
+	 **/
+	 public function getLedgerId() {
+		 return($this->ledgerId);
+	 }
+
 }
 
  ?>
