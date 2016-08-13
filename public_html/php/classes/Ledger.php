558c8a90c0f415991737eb24260fe9030bc00726
Travis Talcott <ttalcott@aquilagroup.com>
2016-08-12 19:15:24 -0600

    added accessor methods https://app.asana.com/0/161549255700582/164161356807352

--- public_html/php/classes/Ledger.php
+++ public_html/php/classes/Ledger.php
@@ -50,6 +50,53 @@ class Ledger {
 		 return($this->ledgerId);
 	 }
 
+	 /**
+	 * mutator method for $ledgerId
+	 *
+	 * @param int|null $newLedgerId new value of ledgerId
+	 * @throws \RangeException if $newLedgerId is not positive
+	 * @throws \TypeError if $newLedgerId is not an integer
+	 **/
+	 public function setLedgerId() {
+		 //verify the ledgerId is positive
+		 
+	 }
+
+	 /**
+	 * accessor method for $ledgerPurchaseId
+	 *
+	 * @return int value of $ledgerPurchaseId
+	 **/
+	 public function getLedgerPurchaseId() {
+		 return($this->ledgerPurchaseId);
+	 }
+
+	 /**
+	 * accessor method for $ledgerAmount
+	 *
+	 * @return decimal value of $ledgerAmount
+	 **/
+	 public function getLedgerAmount() {
+		 return($this->ledgerAmount);
+	 }
+
+	 /**
+	 * accessor method for $ledgerDate
+	 *
+	 * @return \DateTime value of the ledger date and time
+	 **/
+	 public function getLedgerDate() {
+		 return($this->ledgerDate);
+	 }
+
+	 /**
+	 * accessor method for $ledger stripe token
+	 *
+	 * @return string value of $ledgerStripeToken
+	 **/
+	 public function getLedgerStripeToken() {
+		 return($this->ledgerStripeToken);
+	 }
 }
 
  ?>
