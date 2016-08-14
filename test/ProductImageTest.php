766f67b6945d377e5472599e8b36890dcfe67e45
Robert Engelbert <Robe32002@yahoo.com>
2016-08-13 18:56:41 -0600

    Added testGetInvalidImageByProductImageId ProductImageTest.php https://app.asana.com/0/161549255700604/164161356807367

--- test/ProductImageTest.php
+++ test/ProductImageTest.php
@@ -81,4 +81,80 @@ class ProductImageTest extends RootsTableTest{
 		$productImage = new ProductImage(null, $this->CATIMAGEIMAGEID, $this->CATIMAGEPRODUCTID);
 		$productImage->update($this->getPDO());
 	}
+	/**
+	 * test creating a productImage and then deleting it
+	 */
+	public function testDeleteValidProductImage(){
+		//count the number of rows currently in the data base
+		$numRows = $this->getConnection()->getRowCount("productImage");
+
+		//create a new productImage and insert into mySQL
+		$productImage = new ProductIamge(null, $this->CATIMAGEIMAGEID, $this->CATIMAGEPRODUCTID);
+		$productImage->insert($this->getPDO());
+
+		//confirm the row was added, then delete it
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("productImage"));
+		$productImage->delete($this->getPDO());
+
+		//grab data from mySQL and ensure it doesn't exist
+		$pdoProductImage = ProductImage::getProductImageByProductImageImageId($this->getPDO(), $productImage->getProductImageImageId());
+		$this->assertNull($pdoProductImage);
+		$this->assertEquals($numRows, $this->getConnection()->getRowCount("productImage"));
+	}
+	/**
+	 * test deleteing a productImage that does not exist
+	 *
+	 * @expectedException \PDOException
+	 */
+	public function testDeleteInvalidProductImage(){
+		//create a productImage and delete it without inserting it
+		$productImage = new ProductImage(null, $this->CATIMAGEIMAGEID, $this->CATIMAGEPRODUCTID);
+		$productImage->delete($this->getPDO());
+	}
+	/**
+	 * test inserting a productImage and regrabbing it from mySQL
+	 */
+	public function testGetvalidProductImageByProductImageImageId(){
+		//count the number of rows currently in the database
+		$numRows = $this->getConnection()->getRowCount("productImage");
+
+		//create a new productImage and insert into mySQL
+		$productImage = new ProductImage(null, $this->CATIMAGEIMAGEID, $this->CATIMAGEPRODUCTID);
+		$productImage->insert($this->getPDO());
+
+		//grab data from mySQL and enforce that the fields match
+		$pdoProductImage = ProductImage::getProductImageByProductImageImageId($this->getPDO(), $productImage-getProductImageImageId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("productImage"));
+		$this->assertEquals($pdoProductImage->getProductImageImageId(), $this->CATIMAGEIMAGEID);
+		$this->assertEquals($pdoProductImage->getProductImageProductId(), $this->CATIMAGEPRODUCTID);
+	}
+	/**
+	 * test getting an productImage that does not exist
+	 */
+	public function testGetInvalidProductImageByImageId(){
+		//grab an id that exceeds the maximum allowable value
+		$productImage = ProductImage::getProductImageByProductImageImageId($this->getPDO(), RootsTableTest::INVALID_KEY);
+		$this->assertNull($productImage);
+	}
+	/**
+	 * test grabbing a productImage by productId
+	 */
+	public function testGetValidProductImageByProductId(){
+		//count the number of rows currently in the database
+		$numRows = $this->getConnection()->getRowCount("productImage");
+
+		//create a new productImage and insert it into mySQL
+		$pdoProductImage = ProductImage::getProductImageByProductImageProductId($this->getPDO(), $this->CATIMAGEIMAGEID);
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("productImage"));
+		$this->assertEquals($pdoProductImage[0]->getProductImageImageId(), $this->CATIMAGEIMAGEID);
+		$this->assertEquals($pdoProductImage[0]->getProductImageProductId(), $this->CATIMAGEPRODUCTID);
+	}
+	/**
+	 * test grabbing a productImage By productId that does not exist
+	 */
+	public function testGetInvalidProductImageByProductId(){
+		$productImage = ProductImage::getProductImageByProductImageProductId($this->getPDO(), "8925");
+		$this->assertEquals($productImage->getSize(), 0);
+	}
+
 }
