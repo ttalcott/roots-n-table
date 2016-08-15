8bbc9fbcfc8fca3d957bdb72b7628b6760828d15
Travis Talcott <ttalcott@aquilagroup.com>
2016-08-15 13:16:13 -0600

    created LedgerTest file https://app.asana.com/0/161549255700582/164161356807352

---
+++ test/LedgerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Edu\Cnm\Rootstable\Test;
+
+use Edu\Cnm\Rootstable\{Ledger, Purchase};
+
+//grab the project test parameters
+require_once("RootsTableTest.php");
+
+//grab the class under scrutiny
+require_once(dirname(__DIR__) . "/public_html/php/classes/autoload.php");
+
+
+
+ ?>
