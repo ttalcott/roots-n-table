7301bd2d4c4360eed3167132aa25447094761513
Travis Talcott <ttalcott@aquilagroup.com>
2016-08-16 15:25:38 -0600

    added state variables and accessor methods https://app.asana.com/0/161555263951790/164161356807363

--- public_html/php/classes/ProfileImage.php
+++ public_html/php/classes/ProfileImage.php
@@ -3,6 +3,41 @@ namespace Edu\Cnm\Rootstable;
 
 require_once("autoload.php");
 
+/**
+* Profile Image class for Roots 'n Table
+*
+* @author Travis Talcott <ttalcott@lyradevelopment.com>
+* version 1.0.0
+**/
+class ProfileImage {
+	/**
+	* profile id that this image belongs to
+	* @var int $profileImageProfileId
+	**/
+	private $profileImageProfileId;
+	/**
+	* image id that this image belongs to
+	* @var int $profileImageImageId
+	**/
+	private $profileImageImageId;
 
+	/**
+	* accessor method for $profileImageProfileId
+	* @return int value of $profileImageProfileId
+	**/
+	public function getProfileImageProfileId() {
+		return($this->profileImageProfileId);
+	}
 
+	/**
+	
+
+	/**
+	* accessor method for profileImageImageId
+	* @return int value of $profileImageImageId
+	**/
+	public function getProfileImageImageId() {
+		return($this->profileImageImageId);
+	}
+}
  ?>
