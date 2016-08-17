3efd624d315acf25c7b8d9655faa6d4e341228cb
Travis Talcott <ttalcott@aquilagroup.com>
2016-08-17 13:18:26 -0600

    defined state variables and added one accessor https://app.asana.com/0/161555263951790/164161356807369

--- public_html/php/classes/ProductCategory.php
+++ public_html/php/classes/ProductCategory.php
@@ -1,5 +1,33 @@
 <?php
+namespace Edu\Cnm\Rootstable;
 
+require_once("autoload.php");
 
+/**
+* Product Category class for Roots 'n Table
+*
+* @author Travis Talcott <ttalcott@lyradevelopment.com>
+* version 1.0.0
+**/
+class ProductCategory {
+	/**
+	* category id that this productCategory belongs to
+	* @var int $productCategoryCategoryId
+	**/
+	private $productCategoryCategoryId;
+	/**
+	* product id that this productCategory belongs to
+	* @var int $productCategoryProductId
+	**/
+	private $productCategoryProductId;
+
+	/**
+	* accessor method for $productCategoryCategoryId
+	* @return int value of $productCategoryCategoryId
+	**/
+	public function getProductCategoryCategoryId() {
+		return($this->productCategoryCategoryId);
+	}
+}
 
  ?>
