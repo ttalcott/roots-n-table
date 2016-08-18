31cb23e5f4bf23c24b9861522822352b046c0505
Travis Talcott <ttalcott@aquilagroup.com>
2016-08-17 20:05:21 -0600

    created mutator method for ProductCategory https://app.asana.com/0/161555263951790/164161356807369

--- public_html/php/classes/ProductCategory.php
+++ public_html/php/classes/ProductCategory.php
@@ -30,11 +30,45 @@ class ProductCategory {
 	}
 
 	/**
+	* mutator method for productCategoryCategoryId
+	*
+	* @param int $newProductCategoryCategoryId new value of productCategoryCategoryId
+	* @throws \RangeException if $newProductCategoryCategoryId is not positive
+	* @throws \TypeError if $newProductCategoryCategoryId is not an integer
+	**/
+	public function setProductCategoryCategoryId(int $newProductCategoryCategoryId) {
+		//verify $newProductCategoryCategoryId is positive
+		if($newProductCategoryCategoryId <= 0) {
+			throw(new \RangeException("productCategoryCategoryId is not positive"));
+		}
+
+		//convert and store the productCategoryCategoryId
+		$this->productCategoryCategoryId = $newProductCategoryCategoryId;
+	}
+
+	/**
 	* accessor method for $productCategoryProductId
 	* @return int $productCategoryProductId
 	**/
 	public function getProductCategoryProductId() {
 		return($this->productCategoryProductId);
+	}
+
+	/**
+	* mutator method for productCategoryProductId
+	*
+	* @param int $newProductCategoryProductId new value of productCategoryProductId
+	* @throws \RangeException if $newProductCategoryProductId is not positive
+	* @throws \TypeError if $newProductCategoryProductId is not an integer
+	**/
+	public function setProductCategoryProductId(int $newProductCategoryProductId) {
+		//verivy $newProductCategoryProductId is positive
+		if($newProductCategoryProductId <= 0) {
+			throw(new \RangeException("newProductCategoryProductId is not positive"));
+		}
+
+		//convert and store productCategoryProductId
+		$this->productCategoryProductId = $newProductCategoryProductId;
 	}
 }
 
