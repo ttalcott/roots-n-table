1dbdeeb5ad018261122d0c95751ff6fff31d7e4c
Robert Engelbert <Robe32002@yahoo.com>
2016-08-20 10:41:54 -0600

    added first try block for profile.php api https://app.asana.com/0/161555263951790/170023045557763

--- public_html/php/apis/profile.php
+++ public_html/php/apis/profile.php
@@ -12,3 +12,21 @@ use Edu\Cnm\Rootstable;
 if(session_status() !== PHP_SESSION_ACTIVE){
 	session_start();
 }
+
+//I think I need $reply = new stdClass(); and $reply->status = 200;
+
+try{
+	//grab the mySQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/profile.ini");
+
+	//determine which HTTP request method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	//sanitize input
+	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
+
+	//ensure the id is valid
+	if(($method === "GET" || $method === "PUT" ) && (empty($id) === true || $id < 0)){
+		throw(new \InvalidArgumentException("Id cannot be negative or empty", 405));
+	}
+}
