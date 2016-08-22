959e9e536a1207cd6dea84f1dc896e51b6f09f6c
Robert Engelbert <Robe32002@yahoo.com>
2016-08-22 16:19:57 -0600

    Added require_once and doc block with author information https://app.asana.com/0/161555263951790/170023045557781

---
+++ public_html/php/apis/sign-in/index.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once "autoloader.php";
+require_once "/lib/xsrf.php";
+require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+use Edu\Cnm\Rootstable;
+
+/**
+ * api for sign-in
+ *
+ * @author Robert Engelbert <rob@robertengelbert.com
+ */
