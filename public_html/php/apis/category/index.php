a8027b611f62b9eca6425628da73660f20a6469e
Travis Talcott <ttalcott@aquilagroup.com>
2016-08-22 13:30:24 -0600

    created 2 get if statements https://app.asana.com/0/161555263951790/170023045557771

--- public_html/php/apis/category/index.php
+++ public_html/php/apis/category/index.php
@@ -21,6 +21,35 @@ $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
 
+try {
+	//grab the SQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/category.ini");
 
+	//determine what HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	//sanitize all inputs
+	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
+	$categorName = filter_input(INPUT_GET, "categoryName", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+	//handle GET request
+	if($method === GET) {
+		//set XSRF cookie
+		setXsrfCookie();
+
+		//get a category by category id
+		if(empty($id) === false) {
+			$category = Category::getCategoryByCategoryId($pdo, $id);
+			if($category !== null) {
+				$reply->data = $category;
+			}
+		} else if(empty($categoryName) === false) {
+			$categorie = Category::getCategoryByCategoryName($pdo, $categoryName);
+			if($category !== null) {
+				$reply->data = $category;
+			}
+		} else()
+	}
+}
 
  ?>
