bc59339389a81c5663ea9c8d206fe8f377ba9194
Travis Talcott <ttalcott@aquilagroup.com>
2016-08-22 15:02:22 -0600

    added if statement to verify user's profile id https://app.asana.com/0/161555263951790/170023045557761

--- public_html/php/apis/location/index.php
+++ public_html/php/apis/location/index.php
@@ -1,4 +1,6 @@
 <?php
+namespace Edu\Cnm\Rootstable;
+
 require_once("autoload.php");
 require_once("/lib/xsrf.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
@@ -21,7 +23,24 @@ $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
 
+try {
+	//grab the SQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/location.ini");
 
+	//determine what HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	//sanitize all inputs
+	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
+	
+
+	//make sure the user is not using PUT, POST, DELETE when they shouldn't
+	if(($method !== "GET") && ($_SESSION["profile"]->getProfileId() !== $id)) {
+		setXsrfCookie();
+		throw(new \InvalidArgumentException("cannot change these when you are not logged in"));
+	}
+
+}
 
 
  ?>
