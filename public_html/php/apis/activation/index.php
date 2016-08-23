f1f80314bc6e3a3532b00c1e7d97e7dcac2ae0fc
Robert Engelbert <Robe32002@yahoo.com>
2016-08-23 12:50:20 -0600

    Added require_once and doc block https://app.asana.com/0/161555263951790/170023045557808

---
+++ public_html/php/apis/activation/index.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once "autoloader.php";
+require_once "/lib/xsrf.php";
+require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+use Edu\Cnm\Rootstable;
+
+/**
+ * activation api
+ *
+ * @author Robert Engelbert <rob@robertengelbert.com>
+ */
+
