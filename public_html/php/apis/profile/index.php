af479f25c04a2439e1f40f9d1a83802c7e7b4f36
Robert Engelbert <Robe32002@yahoo.com>
2016-08-23 16:56:08 -0600

    reviewed profile and updated it https://app.asana.com/0/161555263951790/170023045557763

--- public_html/php/apis/profile/index.php
+++ public_html/php/apis/profile/index.php
@@ -2,7 +2,7 @@
 
 require_once "autoloader.php";
 require_once "/lib/xsrf.php";
-require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
 use Edu\Cnm\Rootstable;
 
@@ -13,7 +13,7 @@ use Edu\Cnm\Rootstable;
  */
 
 //verify the session, start if not active
-if(session_status() !== PHP_SESSION_ACTIVE){
+if(session_status() !== PHP_SESSION_ACTIVE) {
 	session_start();
 }
 
@@ -22,7 +22,7 @@ $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
 
-try{
+try {
 	//grab the mySQL connection (not sure about this)
 	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/rootstable.ini");
 
@@ -32,30 +32,18 @@ try{
 	//sanitize input
 	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
 	$email = filter_input(INPUT_GET, "email", FILTER_SANITIZE_EMAIL);
-	$firstName = filter_input(INPUT_GET, "firstName" , FILTER_SANITIZE_STRING);
+	$firstName = filter_input(INPUT_GET, "firstName", FILTER_SANITIZE_STRING);
 	$lastName = filter_input(INPUT_GET, "lastName", FILTER_SANITIZE_STRING);
 	$phoneNumber = filter_input(INPUT_GET, "phoneNumber", FILTER_SANITIZE_STRING);
 	$type = filter_input(INPUT_GET, "type", FILTER_SANITIZE_STRING);
 	$userName = filter_input(INPUT_GET, "userName", FILTER_SANITIZE_STRING);
 
 	//ensure the information is valid
-	if(($method === "GET" || $method === "PUT" ) && (empty($id) === true || $id < 0)){
+	if($method === "PUT" && (empty($id) === true || $id < 0)) {
 		throw(new \InvalidArgumentException("Id cannot be negative or empty", 405));
-	}elseif(($method === "GET" || $method === "PUT") && (empty($email) === true)){
-		throw(new \InvalidArgumentException("Value must be valid", 405));
-	}elseif(($method === "GET" || $method === "PUT") && (empty($firstName) === true)){
-		throw(new \InvalidArgumentException("Value must be valid", 405));
-	}elseif(($method === "GET" || $method === "PUT") && (empty($lastName) === true)){
-		throw(new \InvalidArgumentException("Value must be valid", 405));
-	}elseif(($method === "GET" || $method === "PUT") && (empty($phoneNumber) === true)){
-		throw(new \InvalidArgumentException("Value must be valid", 405));
-	}elseif(($method === "GET" || $method === "PUT") && (empty($type) === true)){
-		throw(new \InvalidArgumentException("Value must be valid", 405));
-	}elseif(($method === "GET" || $method === "PUT") && (empty($userName) === true)){
-		throw(new \InvalidArgumentException("Value must be valid", 405));
-	}elseif (($method === "POST" || $method === "DELETE")) {
-			throw(new \Exception("This action is forbidden", 405));
-		}
+	} elseif(($method === "POST" || $method === "DELETE")) {
+		throw(new \Exception("This action is forbidden", 405));
+	}
 
 	//handle GET request
 	if($method === "GET") {
@@ -63,91 +51,83 @@ try{
 		setXsrfCookie("/");
 
 		//get a specific profile
-		if(empty($id) === false) {
-			$profile = Rootstable\Profile::getProfileByProfileId($pdo, $id);
-			if($profile !== null) {
-				$reply->data = $profile;
-			}
-		} elseif(empty($email) === false) {
+		if(empty($email) === false) {
 			$profile = Rootstable\Profile::getProfileByProfileEmail($pdo, $email);
 			if($profile !== null) {
 				$reply->data = $profile;
 			}
-		}elseif(empty($userName) === false){
+		} elseif(empty($userName) === false) {
 			$profile = Rootstable\Profile::getProfileByProfileUserName($pdo, $userName);
-			if($profile !== null){
+			if($profile !== null) {
 				$reply->data = $profile;
 			}
 		}
-	}elseif($method === "PUT"){
-			verifyXsrf();
-			$requestContent = file_get_contents("php://input");
-			$requestObject = json_decode($requestContent);
+	} elseif($method === "PUT") {
+		verifyXsrf();
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
 
-			//make sure profile information is available
-		if(empty($requestObject->profileEmail) === true){
+		//make sure profile information is available
+		if(empty($requestObject->profileEmail) === true) {
 			throw(new \InvalidArgumentException("Insufficient Information", 405));
 		}
-		if(empty($requestObject->profileFirstName) === true){
+		if(empty($requestObject->profileFirstName) === true) {
 			throw(new \InvalidArgumentException("Insufficient Information", 405));
 		}
-		if(empty($requestObject->profileLastName) === true){
+		if(empty($requestObject->profileLastName) === true) {
 			throw(new \InvalidArgumentException("Insufficient Information", 405));
 		}
-		if(empty($requestObject->profilePhoneNumber) === true){
+		if(empty($requestObject->profileType) === true) {
 			throw(new \InvalidArgumentException("Insufficient Information", 405));
 		}
-		if(empty($requestObject->profileType) === true){
+		if(empty($requestObject->profileUserName) === true) {
 			throw(new \InvalidArgumentException("Insufficient Information", 405));
 		}
-		if(empty($requestObject->profileUserName) === true){
-			throw(new \InvalidArgumentException("Insufficient Information", 405));
+	}
+	//preform the put
+	if($method === "PUT") {
+
+		//restrict each user to there own account
+		if(empty($_SESSION["profile"]) === false || $_SESSION["profile"]->getProfileId() !== $id) {
+			throw (new \InvalidArgumentException("You're not authorized to modify this account"));
 		}
+
+		//retrieve the profile to update it
+		$profile = Rootstable\Profile::getProfileByProfileId($pdo, $id);
+		if($profile === null) {
+			throw(new RuntimeException("Profile does not exist", 404));
 		}
-		//preform the put
-		if($method === "PUT"){
 
-			//restrict each user to there own account
-			if(empty($_SESSION["profile"]) === false && $_SESSION["profile"]->getProfileId() === $id){
-				throw (new \InvalidArgumentException("You're not authorized to modify this account"));
-			}
+		//put new profile information into profile and update
+		$profile->setProfileEmail($requestObject->profilelEmail);
+		$profile->setProfileFirstName($requestObject->profielFirstName);
+		$profile->setProfileLastName($requestObject->profileLastName);
+		$profile->setProfilePhoneNumber($requestObject->profilePhoneNumber);
+		$profile->setProfileType($requestObject->profileType);
+		$profile->setProfileUserName($requestObject->profileUserName);
+		//add a if statement to salt and hash the password and set it
 
-			//retrieve the profile to update it
-			$profile = Rootstable\Profile::getProfileByProfileId($pdo, $id);
-			if($profile === null){
-				throw(new RuntimeException("Profile does not exist", 404));
-			}
-
-			//put new profile information into profile and update
-			$profile->setProfileEmail($requestObject->profilelEmail);
-			$profile->setProfileFirstName($requestObject->profielFirstName);
-			$profile->setProfileLastName($requestObject->profileLastName);
-			$profile->setProfilePhoneNumber($requestObject->profilePhoneNumber);
-			$profile->setProfileType($requestObject->profileType);
-			$profile->setProfileUserName($requestObject->profileUserName);
-			//add a if statement to salt and hash the password and set it
-
-			if($requestObject->profilePassword !== null){
-				$hash = hash_pbkdf2("sha512", $requestObject->profilePassword, $profile->getProfileSalt(), 262144, 128);
-				$profile->setProfileHash($hash);
-			}
-			$profile->update($pdo);
-
-			//update username
-			$reply->message = "user information updated";
-		}else{
-			throw(new \InvalidArgumentException("Invalid HTTP method request"));
+		if($requestObject->profilePassword !== null) {
+			$hash = hash_pbkdf2("sha512", $requestObject->profilePassword, $profile->getProfileSalt(), 262144, 128);
+			$profile->setProfileHash($hash);
 		}
-}catch(Exception $exception){
+		$profile->update($pdo);
+
+		//update username
+		$reply->message = "user information updated";
+	} else {
+		throw(new \InvalidArgumentException("Invalid HTTP method request"));
+	}
+} catch(Exception $exception) {
 	$reply->status = $exception->getCode();
 	$reply->message = $exception->getMessage();
 	$reply->trace = $exception->getTraceAsString();
-}catch(TypeError $typeError){
+} catch(TypeError $typeError) {
 	$reply->status = $typeError->getCode();
 	$reply->message = $typeError->getMessage();
 }
 header("Content-type: application/json");
-if($reply->data === null){
+if($reply->data === null) {
 	unset($reply->data);
 }
 
