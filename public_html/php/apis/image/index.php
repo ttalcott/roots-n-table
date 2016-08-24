fcaca290e74a763f6e7d776bfe7b8255e4d79a21
Raul Villareal <rvillarrcal@cnm.edu>
2016-08-24 13:40:42 -0600

    added partial POST Method https://app.asana.com/0/161549255700593/170023045557767

--- public_html/php/apis/image/index.php
+++ public_html/php/apis/image/index.php
@@ -62,29 +62,96 @@ try{
 		if($images !== null) {
 			$reply->data = $images;
 		}
+	}elseif($method === "POST"){
+		verifyXsrf();
+	$requestContent = file_get_contents("php://input");
+	$requestObject = json_decode($requestContent);
+}	if($method ==="POST"){
+		verifyXsrf();
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
+
+		//make sure there is a user to upload the image
+		if(empty($requestObject->profileImageProfileId) === true){
+			throw(new \InvalidArgumentException("The user doesn't exists", 405));
+		}
+
+		//make sure the image path is available
+		if(empty($requestObject->imagePath) === true){
+			throw(new \InvalidArgumentException("The image path is already in use", 405));
+		}
+
+		//make sure the image path is available
+		if(empty($requestObject->imagePath) === true){
+			throw(new \InvalidArgumentException("The image name does not exist", 405));
+		}
+
+		//make sure the image type is a valid one
+		if(empty($requestObject->imageType) === true){
+			throw(new \InvalidArgumentException("The image type should be .png .jpg or .jpeg"));
+		}
+	}if($method === "POST") {
+			// anitize the image
+			$validExtensions = array(".jpg", ",jpeg", ".png");
+			// We'll use strrchr to separate the file names from the extensions. strrchr will look for whatever is after the "." $_FILES is a superglobal that will help us manage the array of uploaded image files
+			$userFileExtension = strrchr($_FILES["userImage"]["name"], ".");
+			if(!in_array($userFileExtension, $validExtensions)){
+				throw(new \InvalidArgumentException("Image has to be .jpg .jpeg or .png"));
+			}
+
+			//image creation when the extension is a valid one
+			if($userFileExtension === ".jpg" || $userFileExtension === ".jpeg"){
+				$userImage = imagecreatefromjpeg($requestObject->image);
+			}elseif($userFileExtension === ".png"){
+				$userImage = imagecreatefrompng($requestObject->image);
+			}
+
+			// now the image needs to be scaled down
+			$sclaedImage = imagescale($userImage, 350);
+
+		//assign a temporary and safe name to the new file
+		$newFileName = round(microtime(true)) . '.' . $scaledImage;
+		move_uploaded_file($_FILES["file"]["tmp_name"], "../img/tempImageDirectory/" . $newFileName);
 	}
 
-		//update reply with exception information
-	} catch(Exception $exception) {
-		$reply->status = $exception->getCode();
-		$reply->message = $exception->getMessage();
-	} catch(TypeError $typeError) {
-		$reply->status = $typeError->getCode();
-		$reply->message = $typeError->getMessage();
+
+
+
+//
+			$newfilename = round(microtime(true)) . '.' . $scaledImage;
+			move_uploaded_file($_FILES["file"]["tmp_name"], "../img/imageDirectory/" . $newfilename);
+			//1) move image to image directory (safe place to work with it) default
+			//2) sanitize image name/type ---------image_type_to_extension & sanitize string?? getimagesize
+//            3) create image--------imagecreatefromjpeg/imagecreatefrompng
+			//4) scale image down to the size I want------imagescale should be in px
+			//5) rename image to something I want
+//            6)imagefoo to save
+		
+		
+//loading of images starts here
+	$type = false;
+	function openImage($file){
+		//detect type and process according to it
+		global ($type);
+		$size = getimagesize($file);
+		switch($size["mime"]){
+			//in case the file is a .jpeg
+			case "image/jpeg":
+				$im = imagecreatefromjpeg($file);
+				break;
+			//in case the file extension is .png
+			case "image/png":
+				$im = imagecreatefrompng($file);
+				break;
+			default:
+				$im = false;
+				break;
+		}
+		return $im;
 	}
 
 
 
-
-
-	//update reply with exception information
-} catch(Exception $exception) {
-	$reply->status = $exception->getCode();
-	$reply->message = $exception->getMessage();
-} catch(TypeError $typeError) {
-	$reply->status = $typeError->getCode();
-	$reply->message = $typeError->getMessage();
-}
 
 
 
@@ -114,4 +181,18 @@ function openImage($file){
 			break;
 	}
 	return $im;
+}
+
+
+
+
+
+
+//update reply with exception information
+} catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+} catch(TypeError $typeError) {
+	$reply->status = $typeError->getCode();
+	$reply->message = $typeError->getMessage();
 }
