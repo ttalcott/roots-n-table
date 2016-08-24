d601181ab559a7e5eaf508230cb69ea5623fd7d0
Robert Engelbert <Robe32002@yahoo.com>
2016-08-23 18:49:23 -0600

    Updated require_once with dirnames https://app.asana.com/0/161555263951790/170023045557808

--- public_html/php/apis/activation/index.php
+++ public_html/php/apis/activation/index.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once "autoloader.php";
-require_once "/lib/xsrf.php";
-require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once(dirname(__DIR__, 2) . "/classes/autoload.php");
+require_once(dirname(__DIR__, 2) . "/lib/xsrf.php");
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
 use Edu\Cnm\Rootstable;
 
