a15655ec5eb70f5ce95ceada2560a7b1936475dc
Travis Talcott <ttalcott@aquilagroup.com>
2016-08-24 13:00:22 -0600

    added dependencies for cart api https://app.asana.com/0/161549255700582/170023045557777

---
+++ public_html/php/apis/cart/index.php
@@ -0,0 +1,22 @@
+<?php
+require_once dirname(__DIR__, 2) . "/classes/autoload.php";
+require_once dirname(__DIR__, 3) . "/lib/xsrf.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+use Edu\Cnm\Rootstable\Category;
+
+/**
+* api for the Category class
+*
+* @author Travis Talcott <ttalcott@lyradevelopment.com>
+**/
+
+//verify the session, if not active start it
+if($session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
