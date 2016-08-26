e0ef08295bf8727de7878a3d10251b8edd9e3a7c
Robert Engelbert <Robe32002@yahoo.com>
2016-08-26 11:44:45 -0600

    added swift mailer to activation api https://app.asana.com/0/161549255700604/170023045557779

--- public_html/php/apis/activation/index.php
+++ public_html/php/apis/activation/index.php
@@ -3,6 +3,8 @@
 require_once(dirname(__DIR__, 2) . "/classes/autoload.php");
 require_once(dirname(__DIR__, 2) . "/lib/xsrf.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once(dirname(__DIR__, 4) . "/vendor/autoload.php");
+
 
 use Edu\Cnm\Rootstable;
 
@@ -52,6 +54,58 @@ try {
 		
 	} elseif($method === "PUT" || $method === "POST" || $method === "DELETE") {
 		throw (new \InvalidArgumentException("This action is not allowed", 405));
+	}
+
+	//swift mailer
+	//create transport
+	$smtp = Swift_SmtpTransport::newInstance("localhost", 25);
+	//create the mailer using the created transport
+	$mailer = Swift_Mailer::newInstance($smtp);
+
+
+	//create swift message
+	$swiftMessage = Swift_message::newInstance();
+
+	//attach the sender to the message
+	//this takes the form of an associtive array where the Email is the key for the real name
+	$swiftMessage->setFrom(["rootsNtable@gmail.com" => "Roots-n-table"]);
+
+	/**
+	 * attach the recipients to the message
+	 * This array can include or omit the recipient's real name
+	 * use the recipient's real name when possible to keep the message from being marked as spam
+	 */
+	$recipients = [$requestObject->profileEmail];
+	$swiftMessage->setTo($recipients);
+
+	//attach the subject line to the message
+	$swiftMessage->setSubject("Confirm your account with Roots-n-table to activate");
+
+	//building the activation link
+	$lastSlash = strrpos($_SERVER["SCRIPT_NAME"], "/");
+	$basePath = substr($_SERVER["SCRIPT_NAME"], 0, $lastSlash + 1);
+	$urlglue = $basePath . "email-confirmation?emailActivation=" . $profileActivationToken;
+
+	$confirmLink = "https://" . $_SERVER["SEVER_NAME"] . $urlglue;
+
+	$message = <<< EOF
+	<h1>Thanks for signing up with Roots-n-table!</h1>
+<p>Visit the following URL to confirm your email and complete the registration process <a href = "$confirmLink">$confirmLink</a></p>
+EOF;
+
+	$swiftMessage->setBody($message, "text/html");
+	$swiftMessage->addPart(html_entity_decode(filter_var($message, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES)), "text/plain");
+
+
+	//send the message
+	$numSent = $mailer->send($swiftMessage, $failedRecipients);
+	/**
+	 * the send method returns the number of recipients that accepted the email
+	 * so, if the number attempted is not the number accepted, throw an exception
+	 */
+	if($numSent !== count($recipients)) {
+		//the $failedRecipients parameter passed in the send() method now contains an array of the emails that failed
+		throw(new \RuntimeException("unable to send email"));
 	}
 } catch(\Exception $exception) {
 	$reply->status = $exception->getCode();
