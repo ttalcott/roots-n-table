5472f5537ea1733d8e37162818b52e4592f82323
Robert Engelbert <Robe32002@yahoo.com>
2016-08-30 15:17:37 -0600

    Changed xsrf path back https://app.asana.com/0/161549255700604/170023045557779

--- public_html/php/apis/sign-up/index.php
+++ public_html/php/apis/sign-up/index.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once(dirname(__DIR__, 2) . "../classes/autoload.php");
-require_once(dirname(__DIR__, 2) . "../lib/xsrf.php");
+require_once(dirname(__DIR__, 2) . "/classes/autoload.php");
+require_once(dirname(__DIR__, 2) . "/lib/xsrf.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 require_once(dirname(__DIR__, 4) . "/vendor/autoload.php");
 
