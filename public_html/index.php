f615ee827db896d5e9d5d4e3774bf2296fe59d7b
Robert Engelbert <Robe32002@yahoo.com>
2016-09-03 11:22:30 -0600

    Added a landing page https://app.asana.com/0/161549255700604/170023045557785

---
+++ public_html/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Roots-n'-table</title>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!--		custom css-->
+		<link href="css/style.css" rel="stylesheet" type="text/css" />
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	</head>
+	<body>
+		<div class="container container-table">
+			<div class="row vertical-center-row">
+				<div class="text-center col-md-4 col-md-offset-4 test">
+					<img src="images/rootsntable-1.png" alt="logo for roots n table" class="img-circle"/>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
