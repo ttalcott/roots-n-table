ee25c8b47cb42f5b75705ef3a7788cf346221486
Robert Engelbert <Robe32002@yahoo.com>
2016-09-04 08:45:49 -0600

    removed icon bars and replaced it with hamburger icon https://app.asana.com/0/161549255700604/170023045557785

--- public_html/angular/pages/shop.php
+++ public_html/angular/pages/shop.php
@@ -17,23 +17,20 @@
 				  crossorigin="anonymous"></script>
 	</head>
 	<body>
-		<nav class="navbar navbar-default">
+		<nav class="navbar navbar-light">
 			<div class="container-fluid">
 				<!-- Brand and toggle get grouped for better mobile display -->
 				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mainNavBar">
+						<span class="glyphicon glyphicon-menu-hamburger"></span>
 					</button>
 					<a class="navbar-brand" href="#">Roots-'n-Table</a>
 				</div>
 
 				<!-- Collect the nav links, forms, and other content for toggling -->
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<div class="collapse navbar-collapse" id="mainNavBar">
 					<ul class="nav navbar-nav">
-						<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+						<li class="nav-item"><a href="#">Link </a></li>
 						<li><a href="#">Link</a></li>
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
