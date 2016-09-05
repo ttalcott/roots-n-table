9b821a02523ff1eed2af458626d8294345d30fe1
Raul Villareal <rvillarrcal@cnm.edu>
2016-09-05 16:01:59 -0600

    added content to route-config.js

--- public_html/angular/route-config.js
+++ public_html/angular/route-config.js
@@ -5,31 +5,55 @@ app.config(function($routeProvider, $locationProvider) {
 	// route for the home page
 		.when('/', {
 			controller: 'homeController',
-			templateUrl: 'angular/views/home.php'
+			templateUrl: 'angular/pages/home.php'
 		})
 
 		// route for the about page
 		.when('/about', {
 			controller: 'aboutController',
-			templateUrl: 'angular/views/about.php'
+			templateUrl: 'angular/pages/about.php'
 		})
 
-		// route for the contact page
-		.when('/contact', {
-			controller: 'contactController',
-			templateUrl: 'angular/views/contact.php'
+		// route for the cart page
+		.when('/cart', {
+			controller: 'cartController',
+			templateUrl: 'angular/pages/cart.php'
 		})
 
-		// route for the education page
-		.when('/education', {
-			controller: 'educationController',
-			templateUrl: 'angular/views/education.php'
+		// route for the categories page
+		.when('/categories', {
+			controller: 'categoriesController',
+			templateUrl: 'angular/pages/categories.php'
 		})
 
-		// route for the solutions page
-		.when('/solutions', {
-			controller: 'solutionsController',
-			templateUrl: 'angular/views/solutions.php'
+		// route for the categoryfoo page
+		.when('/category-foo', {
+			controller: 'categoryfooController',
+			templateUrl: 'angular/pages/category-foo.php'
+		})
+
+		// route for the shop page
+		.when('/shop', {
+			controller: 'shopController',
+			templateUrl: 'angular/pages/shop.php'
+		})
+
+		// route for the sign up/in page
+		.when('/signup', {
+			controller: 'signupController',
+			templateUrl: 'angular/pages/signup.php'
+		})
+
+		// route for the vendor foo page
+		.when('/vendorfoo', {
+			controller: 'vendorfooController',
+			templateUrl: 'angular/pages/vendor-foo.php'
+		})
+
+		// route for the vendors page
+		.when('/vendors', {
+			controller: 'vendorsController',
+			templateUrl: 'angular/pages/vendors.php'
 		})
 
 		// otherwise redirect to home
