168c4a0f94238db36033ed51416dfae298443ed9
Raul Villareal <rvillarrcal@cnm.edu>
2016-09-05 14:47:26 -0600

    modified head utils file

--- public_html/php/partials/head-utils.php
+++ public_html/php/partials/head-utils.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!--<!DOCTYPE html>
 <html lang="en" ng-app="MypwpApp">
 	<head>
 		<!-- The 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
@@ -7,7 +7,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
 		<!-- set base for relative links - to enable pretty URLs -->
-		<base href="<?php echo dirname($_SERVER["PHP_SELF"]) . "/";?>">
+		<base href="<?php /*echo dirname($_SERVER["PHP_SELF"]) . "/";*/?>">
 
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -22,12 +22,36 @@
 		<link rel="stylesheet" href="css/style.css" type="text/css">
 
 		<!--Angular JS Libraries-->
-		<?php $ANGULAR_VERSION = "1.5.8";?>
-		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/<?php echo $ANGULAR_VERSION;?>/angular.min.js"></script>
-		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/<?php echo $ANGULAR_VERSION;?>/angular-messages.min.js"></script>
-		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/<?php echo $ANGULAR_VERSION;?>/angular-route.js"></script>
-		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/<?php echo $ANGULAR_VERSION;?>/angular-animate.js"></script>
+		<?php /*$ANGULAR_VERSION = "1.5.8";*/?>
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/<?php /*echo $ANGULAR_VERSION;*/?>/angular.min.js"></script>
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/<?php /*echo $ANGULAR_VERSION;*/?>/angular-messages.min.js"></script>
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/<?php /*echo $ANGULAR_VERSION;*/?>/angular-route.js"></script>
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/<?php /*echo $ANGULAR_VERSION;*/?>/angular-animate.js"></script>
 		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/1.3.3/ui-bootstrap-tpls.min.js"></script>
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Roots-'n-table</title>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!--		custom css-->
+		<link href="css/style.css" rel="stylesheet" type="text/css"/>
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
+	</head>
+
 
 		<!--Load our angular files-->
 		<script src="angular/mypwp-app.js"></script>
@@ -40,5 +64,5 @@
 		<script src="angular/controllers/education-controller.js"></script>
 		<script src="angular/controllers/solutions-controller.js"></script>
 
-		<title>wev-a-tion</title>
+		<title>roots'n table</title>
 	</head>
