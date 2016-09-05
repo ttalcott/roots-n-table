298a69d95c0d2a533dfd0138a54e174a4858ec41
Raul Villareal <rvillarrcal@cnm.edu>
2016-09-05 15:22:01 -0600

    created pages files (cart, ategory, category foo, sign-up, vendor, vendor foo)

--- public_html/php/partials/header.php
+++ public_html/php/partials/header.php
@@ -1,7 +1,32 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: STEMulus
- * Date: 9/5/2016
- * Time: 2:30 PM
- */
+<header ng-controller="navController">
+
+	<!-- bootstrap breakpoint directive to control collapse behavior -->
+	<bootstrap-breakpoint></bootstrap-breakpoint>
+
+	<nav class="navbar navbar-default navbar-fixed-top navbar-inverse">
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" aria-expanded="false" ng-click="navCollapsed = !navCollapsed">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="index"><img src="images/rootsntable-1.png" width="75"></a>
+			</div>
+
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" uib-collapse="navCollapsed">
+
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="index">Home</a></li>
+					<li><a href="about">About</a></li>
+					<li><a href="solutions">Solutions</a></li>
+					<li><a href="education">Education</a></li>
+					<li><a href="contact">Contact</a></li>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</div><!-- /.container-fluid -->
+	</nav>
+</header>
