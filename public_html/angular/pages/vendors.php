b788df84f9fb0461c4b98e5e7b0ddad5115eaa9b
Robert Engelbert <Robe32002@yahoo.com>
2016-09-06 09:22:00 -0600

    Added categories to vendor page https://app.asana.com/0/161549255700604/170023045557785

--- public_html/angular/pages/vendors.php
+++ public_html/angular/pages/vendors.php
@@ -77,21 +77,41 @@
 		</div>
 		<div class="container">
 			<div class="row">
-				<div class="col-sm-6 col-md-6">
+				<div class="col-sm-6 col-md-3">
 					<div class="thumbnail">
 						<img src="../../images/mixed-fruit.jpeg" alt="Mixed fruit"/>
 						<div class="caption">
-							<h3>Fruit</h3>
+							<h3>product1</h3>
 							<p></p>
 							<p><a href="#" class="btn btn-danger" role="button">Want some?</a></p>
 						</div>
 					</div>
 				</div>
-				<div class="col-sm-6 col-md-6">
+				<div class="col-sm-6 col-md-3">
 					<div class="thumbnail">
 						<img src="../../images/vegetables.jpeg" alt="picture of vegatables"/>
 						<div class="caption">
-							<h3>Vegetables</h3>
+							<h3>product2</h3>
+							<p></p>
+							<p><a href="#" class="btn btn-danger" role="button">Want some?</a></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-3">
+					<div class="thumbnail">
+						<img src="../../images/vegetables.jpeg" alt="picture of vegatables"/>
+						<div class="caption">
+							<h3>Product3</h3>
+							<p></p>
+							<p><a href="#" class="btn btn-danger" role="button">Want some?</a></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-3">
+					<div class="thumbnail">
+						<img src="../../images/vegetables.jpeg" alt="picture of vegatables"/>
+						<div class="caption">
+							<h3>Product4</h3>
 							<p></p>
 							<p><a href="#" class="btn btn-danger" role="button">Want some?</a></p>
 						</div>
