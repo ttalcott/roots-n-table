eceb20052e68645eeffa278e45bfc329d4170e67
Raul Villareal <rvillarrcal@cnm.edu>
2016-09-06 09:01:42 -0600

    modified navbar in header

--- public_html/php/partials/header.php
+++ public_html/php/partials/header.php
@@ -3,27 +3,41 @@
 	<!-- bootstrap breakpoint directive to control collapse behavior -->
 	<bootstrap-breakpoint></bootstrap-breakpoint>
 
-	<nav class="navbar navbar-default navbar-fixed-top navbar-inverse">
+	<nav class="navbar navbar-default">
 		<div class="container-fluid">
 			<!-- Brand and toggle get grouped for better mobile display -->
 			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" aria-expanded="false" ng-click="navCollapsed = !navCollapsed">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mainNavBar">
+					<span class="glyphicon glyphicon-menu-hamburger"></span>
 				</button>
-				<a class="navbar-brand" href="index"><img src="images/rootsntable-1.png" width="75"></a>
+				<a class="navbar-brand" href="#">Roots-'n-Table</a>
 			</div>
 
 			<!-- Collect the nav links, forms, and other content for toggling -->
-			<div class="collapse navbar-collapse" uib-collapse="navCollapsed">
+			<div class="collapse navbar-collapse" id="mainNavBar">
+				<ul class="nav navbar-nav">
+					<li class="nav-item"><a href="#">Home</a></li>
+				</ul>
+				<form class="navbar-form navbar-left">
+					<div class="form-group">
+						<input type="text" class="form-control" placeholder="Search">
+					</div>
+					<button type="submit" class="btn btn-default">Search</button>
+				</form>
 
 				<ul class="nav navbar-nav navbar-right">
-					<li><a href="index">Home</a></li>
-					<li><a href="about">About</a></li>
-					<li><a href="shop">Shop</a></li>
-					<li><a href="signup">Sign up/in</a></li>
+					<li><a href="#">Sign in/Sign up</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+							aria-expanded="false">Shop<span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Fruit</a></li>
+							<li><a href="#">Vegetables</a></li>
+							<li><a href="#">About us</a></li>
+							<li role="separator" class="divider"></li>
+							<li><a href="#">Contact</a></li>
+						</ul>
+					</li>
 				</ul>
 			</div><!-- /.navbar-collapse -->
 		</div><!-- /.container-fluid -->
