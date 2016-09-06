0f44dc8febdf04be3083c64114a8da9feb726eb8
Robert Engelbert <Robe32002@yahoo.com>
2016-09-06 16:33:45 -0600

    Added the beginning of a footer https://app.asana.com/0/161549255700604/170023045557785

---
+++ public_html/php/partials/footer.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>
+			footer
+		</title>
+	</head>
+	<body>
+		<p>words</p>
+		<footer>
+			&copy; 2016 Roots-'n-table
+		</footer>
+	</body>
+</html>
