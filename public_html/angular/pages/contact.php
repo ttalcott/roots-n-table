b4aabef71a0fe3c9d38e79ec59600aa2800d5c0b
Robert Engelbert <Robe32002@yahoo.com>
2016-09-07 09:55:37 -0600

    Added the html an bootstrap for the contact form https://app.asana.com/0/161549255700604/170023045557785

--- public_html/angular/pages/contact.php
+++ public_html/angular/pages/contact.php
@@ -1,11 +1,47 @@
 <!DOCTYPE html>
-	<html lang="en">
+<html lang="en">
 	<head>
 		<meta charset="UTF-8">
 		<title>contact</title>
-		<link href="../../css/style.css" rel="stylesheet" type="text/css" />
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<link href="../../css/style.css" rel="stylesheet" type="text/css"/>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
 	</head>
 	<body>
+		<div class="col-md-2"></div>
+		<div class="col-md-8">
+			<form class="form-horizontal">
+				<div class="form-group">
+					<label for="textInput"></label>
+					<input type="text" class="form-control" id="textInput" placeholder="Firstname"/>
+				</div>
+				<div class="form-group">
+					<label for="textInput"></label>
+					<input type="text" class="form-control" id="textInput" placeholder="Lastname"/>
+				</div>
+				<div class="form-group">
+					<label for="emailInput"></label>
+					<input type="email" class="form-control" id="emailInput" placeholder="Email"/>
+				</div>
+				<div class="form-group">
+					<label for="textInput"></label>
+					<textarea class="form-control" rows="3" placeholder="Leave a message"></textarea>
+				</div>
+				<div class="form-group">
+					<div class="col-sm-offset-6">
+						<button type="button" class="btn btn-danger">Contact</button>
+					</div>
+				</div>
+			</form>
+		</div>
+		<div class="col-md-2"></div>
 
 	</body>
 </html>
