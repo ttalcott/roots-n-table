55b401dd17f71dbdcd16990c12dd4b53c377fcbb
Robert Engelbert <Robe32002@yahoo.com>
2016-09-07 10:16:06 -0600

    Added the footer to the contact form. https://app.asana.com/0/161549255700604/170023045557785

--- public_html/angular/pages/contact.php
+++ public_html/angular/pages/contact.php
@@ -66,33 +66,46 @@
 		</div>
 
 		<!--		contact form-->
-		<div class="col-md-2"></div>
-		<div class="col-md-8">
-			<form class="form-horizontal">
-				<div class="form-group">
-					<label for="textInput"></label>
-					<input type="text" class="form-control" id="textInput" placeholder="Firstname"/>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-2"></div>
+				<div class="col-md-8">
+					<form class="form-horizontal">
+						<div class="form-group">
+							<label for="textInput"></label>
+							<input type="text" class="form-control" id="textInput" placeholder="Firstname"/>
+						</div>
+						<div class="form-group">
+							<label for="textInput"></label>
+							<input type="text" class="form-control" id="textInput" placeholder="Lastname"/>
+						</div>
+						<div class="form-group">
+							<label for="emailInput"></label>
+							<input type="email" class="form-control" id="emailInput" placeholder="Email"/>
+						</div>
+						<div class="form-group">
+							<label for="textInput"></label>
+							<textarea class="form-control" rows="3" placeholder="Leave a message"></textarea>
+						</div>
+						<div class="form-group">
+							<div class="col-sm-offset-6">
+								<button type="button" class="btn btn-danger">Contact</button>
+							</div>
+						</div>
+					</form>
 				</div>
-				<div class="form-group">
-					<label for="textInput"></label>
-					<input type="text" class="form-control" id="textInput" placeholder="Lastname"/>
-				</div>
-				<div class="form-group">
-					<label for="emailInput"></label>
-					<input type="email" class="form-control" id="emailInput" placeholder="Email"/>
-				</div>
-				<div class="form-group">
-					<label for="textInput"></label>
-					<textarea class="form-control" rows="3" placeholder="Leave a message"></textarea>
-				</div>
-				<div class="form-group">
-					<div class="col-sm-offset-6">
-						<button type="button" class="btn btn-danger">Contact</button>
+				<div class="col-md-2"></div>
+			</div>
+		</div>
+
+		<footer class="foot">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-xs-12">
+						&copy; 2016 Roots-'n-table
 					</div>
 				</div>
-			</form>
-		</div>
-		<div class="col-md-2"></div>
-
+			</div>
+		</footer>
 	</body>
 </html>
