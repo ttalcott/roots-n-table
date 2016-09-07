cb2185f8f99f3aa6a6e15b5e26600bc3f358ca30
Raul Villareal <rvillarrcal@cnm.edu>
2016-09-07 09:00:57 -0600

    fixed errors in head-utils https://app.asana.com/0/161555263951790/170023045557787

--- public_html/php/partials/header.php
+++ public_html/php/partials/header.php
@@ -1,4 +1,4 @@
-<header ng-controller="navController">
+<header ng-controller= "navController">
 
 	<!-- bootstrap breakpoint directive to control collapse behavior -->
 	<bootstrap-breakpoint></bootstrap-breakpoint>
