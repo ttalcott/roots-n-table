ac324305151308b37e8e9351d4e07c3805bdfe64
Robert Engelbert <Robe32002@yahoo.com>
2016-09-07 08:57:13 -0600

    Added a bootstrap classes to the footer https://app.asana.com/0/161549255700604/170023045557785

--- public_html/php/partials/footer.php
+++ public_html/php/partials/footer.php
@@ -1,4 +1,10 @@
 <!--partial for footer with class foot for styles-->
 		<footer class="foot">
-			&copy; 2016 Roots-'n-table
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-xs-12">
+						&copy; 2016 Roots-'n-table
+					</div>
+				</div>
+			</div>
 		</footer>
