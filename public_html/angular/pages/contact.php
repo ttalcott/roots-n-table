da1063c3ec00bd419ff64713a4474d9ffeab2b49
Robert Engelbert <Robe32002@yahoo.com>
2016-09-07 09:22:24 -0600

    Added the basic structure for the contact form https://app.asana.com/0/161549255700604/170023045557785

---
+++ public_html/angular/pages/contact.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>contact</title>
+		<link href="../../css/style.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+
+	</body>
+</html>
