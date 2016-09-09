989fa601bac1d1ee3e6a4bbed0594c304ba54b40
Raul Villareal <rvillarrcal@cnm.edu>
2016-09-09 10:39:17 -0600

    added upload files service to products and signup controllers  https://app.asana.com/0/161555263951790/170023045557787

--- public_html/angular/controllers/signup-controller.js
+++ public_html/angular/controllers/signup-controller.js
@@ -1,4 +1,20 @@
 app.controller('signup', function($scope){
 
+	//service to upload files
+//@see https://github.com/danialfarid/ng-file-upload
+
+	Upload.upload({
+		url: '../../php/api/image/index.php',
+		method: 'POST',
+		data: {file: productImage, productDescription: productDescription}
+	}).then(function(result) {
+// PROMISE ACCEPTED: yay!
+	}, function(result) {
+// PROMISE REJECTED: no! :'(
+	}, function(event) {
+// CUSTOM "PROMISE": percent uploaded
+		var percentage = 100.0 * event.loaded / event.total;
+	});
+
 });
 
