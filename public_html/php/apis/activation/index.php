ea75ba7a49a4405ee6976d5837c90578ca29de58
Travis Talcott <ttalcott@aquilagroup.com>
2016-09-09 10:48:25 -0600

    added a few methods to check for emptiness https://app.asana.com/0/161555263951790/170023045557808

--- public_html/php/apis/activation/index.php
+++ public_html/php/apis/activation/index.php
@@ -6,7 +6,7 @@ require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 require_once(dirname(__DIR__, 4) . "/vendor/autoload.php");
 
 
-use Edu\Cnm\Rootstable;
+use Edu\Cnm\Rootstable\Profile;
 
 /**
  * activation api
@@ -32,7 +32,7 @@ try {
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
 	//sanitize activation token
-	$activate = filter_input(INPUT_GET, "activate", FILTER_SANITIZE_STRING);
+	$activate = filter_input(INPUT_GET, "activate", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 
 	if(($method === "GET") && (empty($activate) === true)) {
 		throw(new \InvalidArgumentException("Invalid information", 405));
@@ -43,68 +43,28 @@ try {
 		//set XSRF cookie
 		setXsrfCookie("/");
 
+		if(empty($activate) === true) {
+			throw(new \InvalidArgumentException("No profile activation token present"));
+		}
+
 		//get by activation token
 		if(empty($activate) === false) {
 			$profile = Rootstable\Profile::getProfileByProfileActivationToken($pdo, $activate);
-		}//if activate is not null then null it out
+		}
+
+		if(empty($profile) === true) {
+			throw(new \RuntimeException("Profile not found"));
+		}
+
+		//if activate is not null then null it out
 		if($profile !== null) {
 			$profile->setProfileActivationToken(null);
+			$profile->update($pdo);
 			$reply->message = "Thank you for activating your account";
 		}
-		
+
 	} elseif($method === "PUT" || $method === "POST" || $method === "DELETE") {
 		throw (new \InvalidArgumentException("This action is not allowed", 405));
-	}
-
-	//swift mailer
-	//create transport
-	$smtp = Swift_SmtpTransport::newInstance("localhost", 25);
-	//create the mailer using the created transport
-	$mailer = Swift_Mailer::newInstance($smtp);
-
-
-	//create swift message
-	$swiftMessage = Swift_message::newInstance();
-
-	//attach the sender to the message
-	//this takes the form of an associtive array where the Email is the key for the real name
-	$swiftMessage->setFrom(["rootsNtable@gmail.com" => "Roots-n-table"]);
-
-	/**
-	 * attach the recipients to the message
-	 * This array can include or omit the recipient's real name
-	 * use the recipient's real name when possible to keep the message from being marked as spam
-	 */
-	$recipients = [$requestObject->profileEmail];
-	$swiftMessage->setTo($recipients);
-
-	//attach the subject line to the message
-	$swiftMessage->setSubject("Confirm your account with Roots-n-table to activate");
-
-	//building the activation link
-	$farmScript = $_SERVER["SCRIPT_NAME"];
-	$smtp = dirname($farmScript, 2) . "/activation/?activate=" . $activate;
-
-	$confirmLink = "https://" . $_SERVER["SEVER_NAME"] . $smtp;
-
-	$message = <<< EOF
-	<h1>Thanks for signing up with Roots-n-table!</h1>
-<p>Visit the following URL to confirm your email and complete the registration process <a href = "$confirmLink">$confirmLink</a></p>
-EOF;
-
-	$swiftMessage->setBody($message, "text/html");
-	$swiftMessage->addPart(html_entity_decode(filter_var($message, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES)), "text/plain");
-
-
-	//send the message
-	$numSent = $mailer->send($swiftMessage, $failedRecipients);
-	/**
-	 * the send method returns the number of recipients that accepted the email
-	 * so, if the number attempted is not the number accepted, throw an exception
-	 */
-	if($numSent !== count($recipients)) {
-		//the $failedRecipients parameter passed in the send() method now contains an array of the emails that failed
-		throw(new \RuntimeException("unable to send email"));
 	}
 } catch(\Exception $exception) {
 	$reply->status = $exception->getCode();
