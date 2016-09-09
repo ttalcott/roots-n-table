4dbb696157ad7d8334a5d7d94feaf1456e09bfac
Raul Villareal <rvillarrcal@cnm.edu>
2016-09-09 10:23:59 -0600

    created file upload service  https://app.asana.com/0/161555263951790/170023045557787

---
+++ public_html/angular/services/upload-service.js
@@ -0,0 +1,15 @@
+//service to upload files
+//@see https://github.com/danialfarid/ng-file-upload
+
+Upload.upload({
+	url: '/path/to/image/api/',
+	method: 'POST',
+	data: {file: productImage, productDescription: productDescription}
+}).then(function(result) {
+// PROMISE ACCEPTED: yay!
+}, function(result) {
+// PROMISE REJECTED: no! :'(
+}, function(event) {
+// CUSTOM "PROMISE": percent uploaded
+	var percentage = 100.0 * event.loaded / event.total;
+});
