61ec4311637d7a7c760a40b852bc750e14977277
Robert Engelbert <Robe32002@yahoo.com>
2016-09-10 10:08:39 -0600

    added counter variables and products to the controller https://app.asana.com/0/161549255700604/170023045557785

--- public_html/angular/controllers/shop-controller.js
+++ public_html/angular/controllers/shop-controller.js
@@ -1,4 +1,29 @@
 app.controller('shopController', function($scope){
-
+var counter = 0;
+	$scope.product = {
+		name: 'Corn',
+		description:'Yellow Corn'
+	};
+	$scope.products = [
+		{
+			name: 'Corn',
+			description:'Yellow Corn'
+		},
+		{
+			name:'Apples',
+			description:'Red Apples'
+		},
+		{
+			name:'Kale',
+			description:'Green Kale'
+		}
+	];
+	$scope.addProduct = function(){
+		counter++;
+		$scope.products.push({
+			name:'New product' + counter,
+			description: counter + "New product description"
+		});
+	};
 });
 
