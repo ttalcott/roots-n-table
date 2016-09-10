4bf51cb92d78b977e3bba3017d5fdc03845cb3bb
Raul Villareal <rvillarrcal@cnm.edu>
2016-09-10 13:45:53 -0600

    added image upload controller https://app.asana.com/0/161549255700593/170023045557787

--- public_html/angular/controllers/image-upload-controller.js
+++ public_html/angular/controllers/image-upload-controller.js
@@ -1,3 +1,26 @@
-/**
- * Created by STEMulus on 9/10/2016.
- */
+//inject directives and services.
+var app = angular.module('fileUpload', ['ngFileUpload']);
+
+app.controller('MyCtrl', ['$scope', 'Upload', function ($scope, Upload) {
+	// upload later on form submit or something similar
+	$scope.submit = function() {
+		if($scope.form.file.$valid && $scope.file) {
+			$scope.upload($scope.file);
+		}
+	};
+
+	// upload on file select or drop
+	$scope.upload = function(file) {
+		Upload.upload({
+			url: 'upload/url',
+			data: {file: file, 'username': $scope.username}
+		}).then(function(resp) {
+			console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+		}, function(resp) {
+			console.log('Error status: ' + resp.status);
+		}, function(evt) {
+			var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+			console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+		});
+	};
+}]);
