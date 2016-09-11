be1833f9d200128df0d65b0af0a204622ddcccfc
Robert Engelbert <Robe32002@yahoo.com>
2016-09-11 14:24:49 -0600

    Added .push function to service https://app.asana.com/0/161549255700604/170023045557785

--- public_html/angular/services/product-service.js
+++ public_html/angular/services/product-service.js
@@ -1,21 +1,23 @@
 module.service('Product', ['$rootScope', function($rootScope){
 	var service = {
-			products: [
-				{
-					name: 'Corn',
-					description:'Yellow Corn'
-				},
-				{
-					name:'Apples',
-					description:'Red Apples'
-				},
-				{
-					name:'Kale',
-					description:'Green Kale'
-				}
-			],
-		addProduct: function (product){
-				service.product.push(product);
+		products: [
+			{
+				name: 'Corn',
+				description: 'Yellow Corn'
+			},
+			{
+				name: 'Apples',
+				description: 'Red Apples'
+			},
+			{
+				name: 'Kale',
+				description: 'Green Kale'
+			}
+		],
+			addProduct
+		:
+		function(product) {
+			service.product.push(product);
 			$rootScope.$broadcast('product.update')
 		}
 	};
