5670e7e830fa762436a925428cfc1d580bea788d
Travis Talcott <ttalcott@aquilagroup.com>
2016-09-11 12:38:19 -0600

    moved nullable checks up to be used with the put method as well as the post https://app.asana.com/0/161555263951790/170023045557761

--- public_html/php/apis/location/index.php
+++ public_html/php/apis/location/index.php
@@ -110,6 +110,14 @@ try {
 			throw(new \InvalidArgumentException("No location Zip Code found", 405));
 		}
 
+		//set anything that can be null to null if not provided
+		if(empty($requestObject->locationAttention) === true) {
+			$requestObject->locationAttention = null;
+		}
+		if(empty($requestObject->locationStreetTwo) === true) {
+			$requestObject->locationStreetTwo = null;
+		}
+
 		//preform the actual put
 		if($method === "PUT") {
 			// verifyXsrf();
@@ -137,13 +145,6 @@ try {
 		} else if($method === "POST") {
 			// verifyXsrf();
 
-			//set anything that can be null to null if not provided
-			if(empty($requestObject->locationAttention) === true) {
-				$requestObject->locationAttention = null;
-			}
-			if(empty($requestObject->locationStreetTwo) === true) {
-				$requestObject->locationStreetTwo = null;
-			}
 			//create a new location and insert it into the database
 			$location = new Location(null, $_SESSION["profile"]->getProfileId(), $requestObject->locationAttention, $requestObject->locationCity, $requestObject->locationName, $requestObject->locationState, $requestObject->locationStreetOne, $requestObject->locationStreetTwo, $requestObject->locationZipCode);
 			$location->insert($pdo);
