51b1306ccd4e01049bf783fbf5cc6857ccd47487
Raul Villareal <rvillarrcal@cnm.edu>
2016-09-11 13:27:48 -0600

    modified image upload controller https://app.asana.com/0/161549255700593/170023045557767

--- public_html/angular/controllers/image-upload-controller.js
+++ public_html/angular/controllers/image-upload-controller.js
@@ -1,26 +1,26 @@
 //inject directives and services.
-var app = angular.module('fileUpload', ['ngFileUpload']);
+var app = angular.module('imageUpload', ['ngImageUpload']);
 
 app.controller('MyCtrl', ['$scope', 'Upload', function ($scope, Upload) {
 	// upload later on form submit or something similar
 	$scope.submit = function() {
-		if($scope.form.file.$valid && $scope.file) {
-			$scope.upload($scope.file);
+		if($scope.form.image.$valid && $scope.image) {
+			$scope.upload($scope.image);
 		}
 	};
 
-	// upload on file select or drop
-	$scope.upload = function(file) {
+	// upload on image select or drop
+	$scope.upload = function(image) {
 		Upload.upload({
 			url: 'upload/url',
-			data: {file: file, 'username': $scope.username}
+			data: {image: image, 'username': $scope.username}
 		}).then(function(resp) {
-			console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+			console.log('Success ' + resp.config.data.image.name + 'uploaded. Response: ' + resp.data);
 		}, function(resp) {
 			console.log('Error status: ' + resp.status);
 		}, function(evt) {
 			var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-			console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+			console.log('progress: ' + progressPercentage + '% ' + evt.config.data.image.name);
 		});
 	};
 }]);
