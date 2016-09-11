6f03aeacc101710168ac786387d281762d11ff01
Robert Engelbert <Robe32002@yahoo.com>
2016-09-11 14:52:09 -0600

    Added a directive to the vendor directive https://app.asana.com/0/161549255700604/170023045557785

--- public_html/angular/directives/vendor.js
+++ public_html/angular/directives/vendor.js
@@ -1,2 +1,14 @@
 var vendorDir = angular.module("vendorDir", []);
 
+app.directive('vendor', function(){
+	return{
+		restrict:'EA',
+		scope:{
+			title:'@'
+		},
+		replace:true,
+		templateURL:'shop.php',
+		controller: vendorController,
+		link: function($scope, element, attrs){}
+	}
+});
