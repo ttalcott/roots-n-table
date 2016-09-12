d76fa1e9a0e62c4617c002c60662b31bf05dda4d
Raul Villareal <rvillarrcal@cnm.edu>
2016-09-12 13:54:05 -0600

    added content to about page https://app.asana.com/0/161549255700593/170023045557787

--- public_html/angular/pages/about.php
+++ public_html/angular/pages/about.php
@@ -1,18 +1,14 @@
-<?php /*require_once("php/partials/logo.php"); */?>
+<?php /*require_once("php/partials/logo.php"); */ ?>
 <div class="container">
-						<div class="row">
-				<div class="col-md-4">
-					<img src="images/rootsntable-1.png" alt="logo for roots n table" class="img-circle"/>
-					</div>
-				<div class="col-md-8">
-					<h1>About us</h1>
-					<p class="lead">
-						In graecis laboramus mei, pro ea everti alienum.
-						Vix ferri vivendum efficiendi at, et atqui discere mnesarchum mei,
-						et eum altera equidem vituperatoribus. Ei qui wisi tibique placerat.Ne mea sumo consequuntur, sale
-						accusam
-						electram ei usu. Dolore eruditireprimique his te.
-					</p>
-				</div>
-			</div>
+	<div class="row">
+		<div class="col-md-4">
+			<img src="images/rootsntable-1.png" alt="logo for roots n table" class="img-circle"/>
 		</div>
+		<div class="col-md-8">
+			<h1>About us</h1>
+			<p class="lead">
+				Roots 'N Table is mostly a communication tool between local food producers and local  potential consumers. Any local grower, from the biggest to the back yard grower; can connect with any potential client, whether that is a regular individuals or a big restaurants. We intend to alleviate the negative outcomes that comes from our present food productions system. By using this site you will be;  eating fresher, tastier and better; helping your local economic development as well as enhancing your own and your neighbors well-being.
+			</p>
+		</div>
+	</div>
+</div>
--- public_html/angular/pages/shop.php
+++ public_html/angular/pages/shop.php
@@ -1,16 +1,7 @@
 <div class="container">
 	<div class="row">
-		<div class="col-md-4">
+		<div class="col-md-4 col-md-offset-4">
 			<img src="images/rootsntable-1.png" alt="logo for roots n table" class="img-circle"/>
-		</div>
-		<div class="col-md-8">
-			<h1>Our mission</h1>
-			<p class="lead">
-				In graecis laboramus mei, pro ea everti alienum.
-				Vix ferri vivendum efficiendi at, et atqui discere mnesarchum mei,et eum altera equidem
-				vituperatoribus. Ei qui wisi tibique placerat.Ne mea sumo consequuntur, sale accusam electram ei usu.
-				Dolore eruditireprimique his te.
-			</p>
 		</div>
 	</div>
 </div>
