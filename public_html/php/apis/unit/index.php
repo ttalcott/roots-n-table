3081dbda1a5676b700fefa240edd3102f9bb6657
Raul Villareal <rvillarrcal@cnm.edu>
2016-09-12 11:50:51 -0600

    took namespace out from unit api https://app.asana.com/0/161549255700593/170023045557787

--- public_html/php/apis/unit/index.php
+++ public_html/php/apis/unit/index.php
@@ -1,5 +1,5 @@
 <?php
-namespace Edu\Cnm\Rootstable;
+
 require_once(dirname(__DIR__, 2) . "/classes/autoload.php");
 require_once(dirname(__DIR__, 2) . "/lib/xsrf.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
