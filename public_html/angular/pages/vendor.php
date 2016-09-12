2cb6d3fd25f88cab0595bbded340b97b3b4c7b01
Raul Villareal <rvillarrcal@cnm.edu>
2016-09-12 13:31:59 -0600

    medified vendor page, logo https://app.asana.com/0/161549255700593/170023045557787

--- public_html/angular/pages/vendor.php
+++ public_html/angular/pages/vendor.php
@@ -3,15 +3,6 @@
 		<div class="col-md-4">
 			<img src="images/rootsntable-1.png" alt="logo for roots n table" class="img-circle"/>
 		</div>
-		<div class="col-md-8">
-			<h1>Our mission</h1>
-			<p class="lead">
-				In graecis laboramus mei, pro ea everti alienum.
-				Vix ferri vivendum efficiendi at, et atqui discere mnesarchum mei,et eum altera equidem
-				vituperatoribus. Ei qui wisi tibique placerat.Ne mea sumo consequuntur, sale accusam electram ei usu.
-				Dolore eruditireprimique his te.
-			</p>
-		</div>
 	</div>
 </div>
 <div class="container">
