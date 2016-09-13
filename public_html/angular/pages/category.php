e6072bd6e9df8f80b969810e4dcc369f6f7bb799
Robert Engelbert <Robe32002@yahoo.com>
2016-09-13 09:31:17 -0600

    Added a image of a tomato to category https://app.asana.com/0/161549255700604/170023045557785

--- public_html/angular/pages/category.php
+++ public_html/angular/pages/category.php
@@ -4,7 +4,7 @@
 					<img src="images/rootsntable-1.png" alt="logo for roots n table" class="img-circle"/>
 				</div>
 				<div class="col-md-8">
-					<h1>Products</h1>
+					<h1>Fresh vegetables</h1>
 				</div>
 			</div>
 		</div>
@@ -12,10 +12,10 @@
 			<div class="row">
 				<div class="col-sm-6 col-md-3">
 					<div class="thumbnail">
-						<img src="images/mixed-fruit.jpeg" alt="Mixed fruit"/>
+						<img src="images/vegetables.jpeg" alt="Mixed vegetables"/>
 						<div class="caption">
 							<h3>product1</h3>
-							<div ng-controller="ShopController">
+							<!--<div ng-controller="ShopController">
 							<product>
 								<div ng-repeat="product in products">
 									<ul>
@@ -25,8 +25,9 @@
 									</ul>
 								</div>
 							</product>
-							</div>
+							</div>-->
 							<div class="input-group">
+								<img src="images/tomatoes.jpeg" alt="tomatoes" />
 								<span class="input-group-addon">$</span>
 								<input type="text" class="form-control" placeholder="price" />
 							</div>
