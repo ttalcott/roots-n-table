997759879dac09e4e5c50f5ddd59a9b0a8b43a30
Robert Engelbert <Robe32002@yahoo.com>
2016-09-13 16:27:35 -0600

    Added a logo and mission statement https://app.asana.com/0/161549255700604/170023045557785

--- public_html/angular/controllers/shop-controller.js
+++ public_html/angular/controllers/shop-controller.js
@@ -1,39 +1,40 @@
-app.controller('shopController', function($scope){
-	$scope.ButtonClick = function(){
-var counter = 0;
-	$scope.product = {
-		name: 'Corn',
-		description:'Yellow Corn'
-	};
-	$scope.products = [
-		{
-			name: 'Corn',
-			description:'Yellow Corn'
-		},
-		{
-			name:'Apples',
-			description:'Red Apples'
-		},
-		{
-			name:'Kale',
-			description:'Green Kale'
-		}
-	];
-	$scope.addProduct = function(){
-		counter++;
-		$scope.products.push({
-			name:'New product' + counter,
-			description: counter + "New product description"
-		});
-	};
+app.controller('shopController', function($scope) {
 
-	$scope.changeData = function(){
-		counter++;
-		$scope.product = {
-			name: 'Oranges',
-			description: counter + 'Orange oranges'
-		};
-	};
-	}
+	// $scope.ButtonClick = function() {
+	// 	var counter = 0;
+	// 	$scope.product = {
+	// 		name: 'Corn',
+	// 		description: 'Yellow Corn'
+	// 	};
+	// 	$scope.products = [
+	// 		{
+	// 			name: 'Corn',
+	// 			description: 'Yellow Corn'
+	// 		},
+	// 		{
+	// 			name: 'Apples',
+	// 			description: 'Red Apples'
+	// 		},
+	// 		{
+	// 			name: 'Kale',
+	// 			description: 'Green Kale'
+	// 		}
+	// 	];
+	// 	$scope.addProduct = function() {
+	// 		counter++;
+	// 		$scope.products.push({
+	// 			name: 'New product' + counter,
+	// 			description: counter + "New product description"
+	// 		});
+	// 	};
+	//
+	// 	$scope.changeData = function() {
+	// 		counter++;
+	// 		$scope.product = {
+	// 			name: 'Oranges',
+	// 			description: counter + 'Orange oranges'
+	// 		};
+	// 	};
+	// }
 });
 
