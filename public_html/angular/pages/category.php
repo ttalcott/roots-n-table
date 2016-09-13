a3678f1bd24c72ff856ca71f8520c86ac90a2c82
Robert Engelbert <Robe32002@yahoo.com>
2016-09-13 13:11:03 -0600

    added a image of corn https://app.asana.com/0/161549255700604/170023045557785

--- public_html/angular/pages/category.php
+++ public_html/angular/pages/category.php
@@ -22,30 +22,30 @@
 					<p><a href="#" class="btn btn-danger" role="button">Want some?</a></p>
 				</div>
 			</div>
-			<div class="col-sm-6 col-md-4">
-				<div class="thumbnail">
-					<img src="images/tomatoes.jpeg" alt="tomatoes"/>
-					<div class="caption">
-						<h3>product1</h3>
-						<div class="input-group">
-							<span class="input-group-addon">$</span>
-							<input type="text" class="form-control" placeholder="price"/>
-						</div>
-						<p><a href="#" class="btn btn-danger" role="button">Want some?</a></p>
+		</div>
+		<div class="col-sm-6 col-md-4">
+			<div class="thumbnail">
+				<img src="images/tomatoes.jpeg" alt="tomatoes"/>
+				<div class="caption">
+					<h3>product1</h3>
+					<div class="input-group">
+						<span class="input-group-addon">$</span>
+						<input type="text" class="form-control" placeholder="price"/>
 					</div>
+					<p><a href="#" class="btn btn-danger" role="button">Want some?</a></p>
 				</div>
 			</div>
-			<div class="col-sm-6 col-md-4">
-				<div class="thumbnail">
-					<img src="images/Golden-Bantam-Corn.jpg" alt="tomatoes"/>
-					<div class="caption">
-						<h3>Corn</h3>
-						<div class="input-group">
-							<span class="input-group-addon">$</span>
-							<input type="text" class="form-control" placeholder="price"/>
-						</div>
-						<p><a href="#" class="btn btn-danger" role="button">Want some?</a></p>
+		</div>
+		<div class="col-sm-6 col-md-4">
+			<div class="thumbnail">
+				<img src="images/Corn.jpg" alt="tomatoes"/>
+				<div class="caption">
+					<h3>Corn</h3>
+					<div class="input-group">
+						<span class="input-group-addon">$</span>
+						<input type="text" class="form-control" placeholder="price"/>
 					</div>
+					<p><a href="#" class="btn btn-danger" role="button">Want some?</a></p>
 				</div>
 			</div>
 		</div>
