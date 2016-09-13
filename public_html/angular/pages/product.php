57445b1ccbea5034de8792c069534babad300aed
Raul Villareal <rvillarrcal@cnm.edu>
2016-09-13 14:52:04 -0600

    added product.php page https://app.asana.com/0/161549255700593/170023045557787

---
+++ public_html/angular/pages/product.php
@@ -0,0 +1,67 @@
+<div class="container">
+	<div class="row">
+		<div class="col-md-4">
+			<img src="images/rootsntable-1.png" alt="logo for roots n table" class="img-circle"/>
+		</div>
+	</div>
+</div>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-6 col-md-3">
+			<div class="thumbnail">
+				<img src="images/mixed-fruit.jpeg" alt="Mixed fruit"/>
+				<div class="caption">
+					<h3>product1</h3>
+					<p></p>
+					<div class="input-group">
+						<span class="input-group-addon">$</span>
+						<input type="text" class="form-control" placeholder="price" />
+					</div>
+					<p><a href="#" class="btn btn-danger" role="button">Want some?</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-6 col-md-3">
+			<div class="thumbnail">
+				<img src="images/vegetables.jpeg" alt="picture of vegetables"/>
+				<div class="caption">
+					<h3>product2</h3>
+					<p></p>
+					<div class="input-group">
+						<span class="input-group-addon">$</span>
+						<input type="text" class="form-control" placeholder="price" />
+					</div>
+					<p><a href="#" class="btn btn-danger" role="button">Want some?</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-6 col-md-3">
+			<div class="thumbnail">
+				<img src="images/vegetables.jpeg" alt="picture of vegetables"/>
+				<div class="caption">
+					<h3>Product3</h3>
+					<p></p>
+					<div class="input-group">
+						<span class="input-group-addon">$</span>
+						<input type="text" class="form-control" placeholder="price" />
+					</div>
+					<p><a href="#" class="btn btn-danger" role="button">Want some?</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-6 col-md-3">
+			<div class="thumbnail">
+				<img src="images/vegetables.jpeg" alt="picture of vegetables"/>
+				<div class="caption">
+					<h3>Product4</h3>
+					<p></p>
+					<div class="input-group">
+						<span class="input-group-addon">$</span>
+						<input type="text" class="form-control" placeholder="price" />
+					</div>
+					<p><a href="#" class="btn btn-danger" role="button">Want some?</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
