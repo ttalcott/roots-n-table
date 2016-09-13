a5c9b5a14005741e97cb6d9698cf97607633d622
Raul Villareal <rvillarrcal@cnm.edu>
2016-09-13 16:10:24 -0600

    edited vendor page https://app.asana.com/0/161549255700593/170023045557787

--- public_html/angular/pages/vendor.php
+++ public_html/angular/pages/vendor.php
@@ -1,70 +1,17 @@
 <div class="container">
-<!--	<div class="row">
-		<div class="col-md-4">
-			<img src="images/rootsntable-1.png" alt="logo for roots n table" class="img-circle"/>
-		</div>
-	</div>-->
-	<ul>
-		<li ng-repeat="vendor in vendors">{{ profile.profileName }}</li>
-	</ul>
-</div>
-<!--<div class="container">
 	<div class="row">
-		<div class="col-sm-6 col-md-3">
+
+		<!-- ng repeat this -->
+		<div class="col-xs-6 col-sm-4 col-md-3" ng-repeat="vendor in vendors">
 			<div class="thumbnail">
-				<img src="images/mixed-fruit.jpeg" alt="Mixed fruit"/>
+				<img src="images/vegetables.jpeg" alt="vendor selection">
 				<div class="caption">
-					<h3>product1</h3>
-					<p></p>
-					<div class="input-group">
-						<span class="input-group-addon">$</span>
-						<input type="text" class="form-control" placeholder="price" />
-					</div>
+					<h3>{{ vendor.profileName }}</h3>
 					<p><a href="#" class="btn btn-danger" role="button">Want some?</a></p>
 				</div>
 			</div>
+			<!-- end ng repeat -->
+
 		</div>
-		<div class="col-sm-6 col-md-3">
-			<div class="thumbnail">
-				<img src="images/vegetables.jpeg" alt="picture of vegetables"/>
-				<div class="caption">
-					<h3>product2</h3>
-					<p></p>
-					<div class="input-group">
-						<span class="input-group-addon">$</span>
-						<input type="text" class="form-control" placeholder="price" />
-					</div>
-					<p><a href="#" class="btn btn-danger" role="button">Want some?</a></p>
-				</div>
-			</div>
-		</div>
-		<div class="col-sm-6 col-md-3">
-			<div class="thumbnail">
-				<img src="images/vegetables.jpeg" alt="picture of vegetables"/>
-				<div class="caption">
-					<h3>Product3</h3>
-					<p></p>
-					<div class="input-group">
-						<span class="input-group-addon">$</span>
-						<input type="text" class="form-control" placeholder="price" />
-					</div>
-					<p><a href="#" class="btn btn-danger" role="button">Want some?</a></p>
-				</div>
-			</div>
-		</div>
-		<div class="col-sm-6 col-md-3">
-			<div class="thumbnail">
-				<img src="images/vegetables.jpeg" alt="picture of vegetables"/>
-				<div class="caption">
-					<h3>Product4</h3>
-					<p></p>
-					<div class="input-group">
-						<span class="input-group-addon">$</span>
-						<input type="text" class="form-control" placeholder="price" />
-					</div>
-					<p><a href="#" class="btn btn-danger" role="button">Want some?</a></p>
-				</div>
-			</div>
-		</div>
-	</div>-->
+	</div>
 </div>
