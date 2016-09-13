17d9336e0915c5dcb7db2be0e98d3d95e4f0f1af
Robert Engelbert <Robe32002@yahoo.com>
2016-09-13 16:04:10 -0600

    added ng repeat to the div with the col's to get the items to display in there own thumbnails https://app.asana.com/0/161549255700604/170023045557785

--- public_html/angular/pages/category.php
+++ public_html/angular/pages/category.php
@@ -1,72 +1,17 @@
 <div class="container">
 	<div class="row">
-		<div class="col-sm-6 col-md-2">
+
+		<!-- ng repeat this -->
+		<div class="col-xs-6 col-sm-4 col-md-3" ng-repeat="category in categories">
 			<div class="thumbnail">
-				<!--				image here-->
+				<img src="images/vegetables.jpeg" alt="category selection">
 				<div class="caption">
-					<!--					header here-->
-					<ul>
-						<li ng-repeat="category in categories">{{ category.categoryName }}</li>
-					</ul>
-					<div class="input-group">-->
-						<span class="input-group-addon">$</span>
-						<input type="text" class="form-control" placeholder="price"/>
-					</div>
-					<p><a href="#" class="btn btn-danger" role="button">Want some?</a></p>
+					<h3>{{ category.categoryName }}</h3>
+					<p><a href="#" class="btn btn-default" role="button">Select</a></p>
 				</div>
 			</div>
+			<!-- end ng repeat -->
+
 		</div>
 	</div>
-
-	<!--	<div class="row">-->
-	<!--		<div class="col-md-4">-->
-	<!--			<img src="images/rootsntable-1.png" alt="logo for roots n table" class="img-circle"/>-->
-	<!--		</div>-->
-	<!--		<div class="col-md-8">-->
-	<!--			<h1>Fresh vegetables</h1>-->
-	<!--		</div>-->
-	<!--	</div>-->
-	<!--</div>-->
-	<!--<div class="container">-->
-	<!--	<div class="row">-->
-	<!--		<div class="col-sm-6 col-md-4">-->
-	<!--			<div class="thumbnail">-->
-	<!--				<img src="images/tomatoes.jpeg" alt="Mixed vegetables"/>-->
-	<!--				<div class="caption">-->
-	<!--					<h3>Tomato's</h3>-->
-	<!--					<div class="input-group">-->
-	<!--						<span class="input-group-addon">$</span>-->
-	<!--						<input type="text" class="form-control" placeholder="price"/>-->
-	<!--					</div>-->
-	<!--					<p><a href="#" class="btn btn-danger" role="button">Want some?</a></p>-->
-	<!--				</div>-->
-	<!--			</div>-->
-	<!--		</div>-->
-	<!--		<div class="col-sm-6 col-md-4">-->
-	<!--			<div class="thumbnail">-->
-	<!--				<img src="images/potatos.jpeg" alt="tomatoes"/>-->
-	<!--				<div class="caption">-->
-	<!--					<h3>Potato's</h3>-->
-	<!--					<div class="input-group">-->
-	<!--						<span class="input-group-addon">$</span>-->
-	<!--						<input type="text" class="form-control" placeholder="price"/>-->
-	<!--					</div>-->
-	<!--					<p><a href="#" class="btn btn-danger" role="button">Want some?</a></p>-->
-	<!--				</div>-->
-	<!--			</div>-->
-	<!--		</div>-->
-	<!--		<div class="col-sm-6 col-md-4">-->
-	<!--			<div class="thumbnail">-->
-	<!--				<img src="images/Corn.jpg" alt="tomatoes"/>-->
-	<!--				<div class="caption">-->
-	<!--					<h3>Corn</h3>-->
-	<!--					<div class="input-group">-->
-	<!--						<span class="input-group-addon">$</span>-->
-	<!--						<input type="text" class="form-control" placeholder="price"/>-->
-	<!--					</div>-->
-	<!--					<p><a href="#" class="btn btn-danger" role="button">Want some?</a></p>-->
-	<!--				</div>-->
-	<!--			</div>-->
-	<!--		</div>-->
-	<!--	</div>-->
 </div>
