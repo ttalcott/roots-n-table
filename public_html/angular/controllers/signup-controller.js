0e4896f1243484145e8bc2ad13c810205f01f77c
Robert Engelbert <Robe32002@yahoo.com>
2016-09-14 08:05:14 -0600

    Added to the controller for signup https://app.asana.com/0/161549255700604/170023045557785

--- public_html/angular/controllers/signup-controller.js
+++ public_html/angular/controllers/signup-controller.js
@@ -1,4 +1,25 @@
-app.controller('signupController', function($scope){
+// app.controller('signupController', function($scope){
+//
+// });
+app.controller('signupController', ["$scope", "signupService", function($scope, signupService){
+	$scope.signup = [];
+	/*	$scope.sayProduct = function(){
+	 return("");
+	 };*/
 
-});
+	// $scope.getProducts = function(){
+	// 	ProductsService.all()
+	// 		.then(function(result){
+	// 			if(result.data.status === 200){
+	// 				$scope.products = result.data.data;
+	// 			}else{
+	// 				/*$scope.alerts[0] = {type: "danger", msg: result.data.message};*/
+	// 			}
+	// 		});
+	// };
+
+	// if($scope.products.length === 0){
+	// 	$scope.products = $scope.getProducts();
+	// }
+}]);
 
