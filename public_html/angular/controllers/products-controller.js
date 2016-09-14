478f044bdfbd082304d5adab0cf6fdaf7368ffed
Raul Villareal <rvillarrcal@cnm.edu>
2016-09-14 09:46:15 -0600

    pushing message and added a product https://app.asana.com/0/161549255700593/170023045557787

--- public_html/angular/controllers/products-controller.js
+++ public_html/angular/controllers/products-controller.js
@@ -18,8 +18,7 @@ app.controller('productsController', ["$scope", "ProductsService", "cartService"
 		cartService.create(product.productId, quantity)
 			.then(function(result) {
 				if(result.data.status === 200) {
-					$scope.alerts[0] = {type: "success", msg: result.data.m
-						essage};
+					$scope.alerts[0] = {type: "success", msg: result.data.message};
 				} else {
 					$scope.alerts[0] = {type: "danger", msg: result.data.message};
 				}
