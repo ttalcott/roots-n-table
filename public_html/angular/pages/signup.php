18906399d037ea8c59a6a805b256b88a721fccbd
Robert Engelbert <Robe32002@yahoo.com>
2016-09-13 18:38:05 -0600

    Added ng-hide to sign up form https://app.asana.com/0/161549255700604/170023045557785

--- public_html/angular/pages/signup.php
+++ public_html/angular/pages/signup.php
@@ -1,47 +1,49 @@
-		<div class="container">
-			<p class="lead">
-				This form is for anyone that want's to sell on this site but don't worry we DO NOT store any of your personal information in our database.
-			</p>
-		</div>
-		<div class="col-md-2"></div>
-		<div class="col-md-8">
-<!--			Form for farmers-->
+<div class="container">
+	<p class="lead">
+		This form is for anyone that want's to sell on this site but don't worry we DO NOT store any of your personal
+		information in our database.
+	</p>
+</div>
+<div ng-hide="">
+	<div class="col-md-2"></div>
+	<div class="col-md-8">
+		<!--			Form for farmers-->
 		<form>
 			<div class="form-group">
 				<label for="address"></label>
-				<input type="text" class="form-control" id="address" placeholder="Address" />
+				<input type="text" class="form-control" id="address" placeholder="Address"/>
 			</div>
 			<div class="form-group">
 				<label for="country"></label>
-				<input type="text" class="form-control" id="country" placeholder="Country" />
+				<input type="text" class="form-control" id="country" placeholder="Country"/>
 			</div>
 			<div class="form-group">
 				<label for="DOB"></label>
-				<input type="text" class="form-control" id="DOB" placeholder="Date of birth" />
+				<input type="text" class="form-control" id="DOB" placeholder="Date of birth"/>
 			</div>
 			<div class="form-group">
 				<label for="InputEmail"></label>
-				<input type="email" class="form-control" id="InputEmail" placeholder="Email" />
+				<input type="email" class="form-control" id="InputEmail" placeholder="Email"/>
 			</div>
 			<div class="form-group">
 				<label for="firstName"></label>
-				<input type="text" class="form-control" id="firstName" placeholder="Firstname" />
+				<input type="text" class="form-control" id="firstName" placeholder="Firstname"/>
 			</div>
 			<div class="form-group">
 				<label for="LastName"></label>
-				<input type="text" class="form-control" id="LastName" placeholder="Lastname" />
+				<input type="text" class="form-control" id="LastName" placeholder="Lastname"/>
 			</div>
 			<div class="form-group">
 				<label for="PhoneNumber"></label>
-				<input type="text" class="form-control" id="PhoneNumber" placeholder="Phone Number" />
+				<input type="text" class="form-control" id="PhoneNumber" placeholder="Phone Number"/>
 			</div>
 			<div class="form-group">
 				<label for="SSN"></label>
-				<input type="text" class="form-control" id="SSN" placeholder="Social Security Number" />
+				<input type="text" class="form-control" id="SSN" placeholder="Social Security Number"/>
 			</div>
 			<div class="form-group">
 				<label for="UserName"></label>
-				<input type="text" class="form-control" id="UserName" placeholder="User Name" />
+				<input type="text" class="form-control" id="UserName" placeholder="User Name"/>
 			</div>
 			<div class="form-group">
 				<div class="col-sm-offset-6">
@@ -49,39 +51,42 @@
 				</div>
 			</div>
 		</form>
-		</div>
-		<div class="col-md-2"></div>
+	</div>
+</div>
+<div class="col-md-2"></div>
 
 
 <!--		Form for users-->
-		<div class="container">
-			<div class="row">
-				<div class="col-md-2"></div>
-				<div class="col-md-8">
-					<form class="form-horizontal">
-						<div class="form-group">
-							<label for="textInput"></label>
-							<input type="text" class="form-control" id="textInput" placeholder="Firstname"/>
+<div ng-hide="">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8">
+				<form class="form-horizontal">
+					<div class="form-group">
+						<label for="textInput"></label>
+						<input type="text" class="form-control" id="textInput" placeholder="Firstname"/>
+					</div>
+					<div class="form-group">
+						<label for="textInput"></label>
+						<input type="text" class="form-control" id="textInput" placeholder="Lastname"/>
+					</div>
+					<div class="form-group">
+						<label for="textInput"></label>
+						<input type="text" class="form-control" id="textInput" placeholder="Username"/>
+					</div>
+					<div class="form-group">
+						<label for="emailInput"></label>
+						<input type="email" class="form-control" id="emailInput" placeholder="Email"/>
+					</div>
+					<div class="form-group">
+						<div class="col-sm-offset-6">
+							<button type="button" class="btn btn-danger">Sign up</button>
 						</div>
-						<div class="form-group">
-							<label for="textInput"></label>
-							<input type="text" class="form-control" id="textInput" placeholder="Lastname"/>
-						</div>
-						<div class="form-group">
-							<label for="textInput"></label>
-							<input type="text" class="form-control" id="textInput" placeholder="Username"/>
-						</div>
-						<div class="form-group">
-							<label for="emailInput"></label>
-							<input type="email" class="form-control" id="emailInput" placeholder="Email"/>
-						</div>
-						<div class="form-group">
-							<div class="col-sm-offset-6">
-								<button type="button" class="btn btn-danger">Sign up</button>
-							</div>
-						</div>
-					</form>
-				</div>
-				<div class="col-md-2"></div>
+					</div>
+				</form>
 			</div>
+			<div class="col-md-2"></div>
 		</div>
+	</div>
+</div>
