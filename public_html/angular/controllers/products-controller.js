07e2af5837e66fce5c264bcbf634e96e5ca00115
Raul Villareal <rvillarrcal@cnm.edu>
2016-09-14 09:11:22 -0600

    modified product cotnroller and pge https://app.asana.com/0/161549255700593/170023045557787

--- public_html/angular/controllers/products-controller.js
+++ public_html/angular/controllers/products-controller.js
@@ -1,4 +1,4 @@
-app.controller('productsController', ["$scope", "ProductsService", function($scope, ProductsService){
+app.controller('productsController', ["$scope", "ProductsService", "cartService",  function($scope, ProductsService, cartService){
 	$scope.products = [];
 /*	$scope.sayProduct = function(){
 		return("");
@@ -9,6 +9,18 @@ app.controller('productsController', ["$scope", "ProductsService", function($sco
 			.then(function(result){
 				if(result.data.status === 200){
 					$scope.products = result.data.data;
+				}else{
+					$scope.alerts[0] = {type: "danger", msg: result.data.message};
+				}
+			});
+	};
+	
+	//link to cart
+	$scope.addToCart = function(product, quantity){
+		cartService.create(product,quantity)
+			.then(function(result) {
+				if(result.data.status === 200){
+					$scope.alerts[0] = {type: "success", msg: result.data.message};
 				}else{
 					$scope.alerts[0] = {type: "danger", msg: result.data.message};
 				}
