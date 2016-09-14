207bb740956d2499ed13926bbc946cf4f34d4cf4
Raul Villareal <rvillarrcal@cnm.edu>
2016-09-14 09:23:44 -0600

    cart loads info now https://app.asana.com/0/161549255700593/170023045557787

--- public_html/angular/controllers/products-controller.js
+++ public_html/angular/controllers/products-controller.js
@@ -1,33 +1,31 @@
-app.controller('productsController', ["$scope", "ProductsService", "cartService",  function($scope, ProductsService, cartService){
+app.controller('productsController', ["$scope", "ProductsService", "cartService", function($scope, ProductsService, cartService) {
+	$scope.alerts = [];
 	$scope.products = [];
-/*	$scope.sayProduct = function(){
-		return("");
-	};*/
 
-	$scope.getProducts = function(){
+	$scope.getProducts = function() {
 		ProductsService.all()
-			.then(function(result){
-				if(result.data.status === 200){
+			.then(function(result) {
+				if(result.data.status === 200) {
 					$scope.products = result.data.data;
-				}else{
+				} else {
 					$scope.alerts[0] = {type: "danger", msg: result.data.message};
 				}
 			});
 	};
-	
+
 	//link to cart
-	$scope.addToCart = function(product, quantity){
+	$scope.addToCart = function(product, quantity) {
 		cartService.create(product.productId, quantity)
 			.then(function(result) {
-				if(result.data.status === 200){
+				if(result.data.status === 200) {
 					$scope.alerts[0] = {type: "success", msg: result.data.message};
-				}else{
+				} else {
 					$scope.alerts[0] = {type: "danger", msg: result.data.message};
 				}
 			});
 	};
 
-	if($scope.products.length === 0){
+	if($scope.products.length === 0) {
 		$scope.products = $scope.getProducts();
 	}
 }]);
