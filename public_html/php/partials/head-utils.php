6c0454343bb101af7debd87afb1606456df0b8f7
Travis Talcott <ttalcott@aquilagroup.com>
2016-09-16 11:45:49 -0600

    corrected base href

--- public_html/php/partials/head-utils.php
+++ public_html/php/partials/head-utils.php
@@ -8,7 +8,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
 		<!-- set base for relative links - to enable pretty URLs -->
-		<base href="<?php echo dirname($_SERVER["PHP_SELF"]) . "/";?>">
+		<base href="/">
 
 		<!-- FontAwesome -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
